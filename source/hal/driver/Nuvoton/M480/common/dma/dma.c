94c752439ca2b9feab89e0755fcdf45cdb07d007
Yongxiang Liang <tanekliang@gmail.com>
2021-04-14 15:32:01 +0800

    [hal/m484] add dma support

---
+++ source/hal/driver/Nuvoton/M480/common/dma/dma.c
@@ -0,0 +1,159 @@
+/*****************************************************************************
+ *   Copyright(C)2009-2019 by VSF Team                                       *
+ *                                                                           *
+ *  Licensed under the Apache License, Version 2.0 (the "License");          *
+ *  you may not use this file except in compliance with the License.         *
+ *  You may obtain a copy of the License at                                  *
+ *                                                                           *
+ *     http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                           *
+ *  Unless required by applicable law or agreed to in writing, software      *
+ *  distributed under the License is distributed on an "AS IS" BASIS,        *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and      *
+ *  limitations under the License.                                           *
+ *                                                                           *
+ ****************************************************************************/
+
+
+/*============================ INCLUDES ======================================*/
+#include "./dma.h"
+
+
+/*============================ MACROS ========================================*/
+#define M484_CHANNAL_MAX    16
+/*============================ MACROFIED FUNCTIONS ===========================*/
+/*============================ TYPES =========================================*/
+/*============================ GLOBAL VARIABLES ==============================*/
+/*============================ LOCAL VARIABLES ===============================*/
+static m484_dma_channel_t __channals[M484_CHANNAL_MAX];
+/*============================ PROTOTYPES ====================================*/
+/*============================ IMPLEMENTATION ================================*/
+
+vsf_err_t m484_dma_init(m484_dma_cfg_t *cfg_ptr)
+{
+    VSF_HAL_ASSERT(cfg_ptr != NULL);
+
+    bool state = m480_reg_unlock();
+
+    // enable dma clock
+    CLK->AHBCLK |= CLK_AHBCLK_PDMACKEN_Msk;
+
+    // reset pdma
+    SYS->IPRST0  |= SYS_IPRST0_PDMARST_Msk;
+    SYS->IPRST0  &= ~SYS_IPRST0_PDMARST_Msk;
+
+    // enable dma interrupt and update interrupt priority
+    NVIC_SetPriority(PDMA_IRQn, (uint32_t)cfg_ptr->prio);
+    NVIC_EnableIRQ(PDMA_IRQn);
+    //NVIC_SetPendingIRQ(PDMA_IRQn);
+
+    m480_reg_lock(state);
+
+    return VSF_ERR_NONE;
+}
+
+vsf_err_t m484_dma_channel_config(uint8_t channel, m484_dma_channel_cfg_t *cfg_ptr)
+{
+    VSF_HAL_ASSERT(channel < M484_CHANNAL_MAX);
+    VSF_HAL_ASSERT(cfg_ptr != NULL);
+    VSF_HAL_ASSERT((0 < cfg_ptr->count) && (cfg_ptr->count <= UINT16_MAX + 1));
+    VSF_HAL_ASSERT(cfg_ptr->src_address != NULL);
+    VSF_HAL_ASSERT(cfg_ptr->dst_address != NULL);
+
+    if (PDMA->TRGSTS & (1 << channel)) {
+        return VSF_ERR_FAIL;
+    }
+
+    uint32_t peripheral_index = cfg_ptr->mode & PDMA_REQSEL0_3_REQSRC0_Msk;
+
+    PDMA->CHCTL &= ~(1 << channel);     // disable current channel
+    PDMA->TDSTS = 1 << channel;         // clean last transfer flag
+
+    PDMA->DSCT[channel].CTL   = (0x01 << PDMA_DSCT_CTL_OPMODE_Pos)    |   // Basic mode
+                                (0x01 << PDMA_DSCT_CTL_TXTYPE_Pos)    |   // signal transfer type
+                                (0x00 << PDMA_DSCT_CTL_TBINTDIS_Pos)  |   // Table interrupt Disabled
+                                (0x00 << PDMA_DSCT_CTL_STRIDEEN_Pos)  |   // Stride transfer mode Disabled
+                                cfg_ptr->mode & (PDMA_DSCT_CTL_TXWIDTH_Msk | PDMA_DSCT_CTL_SAINC_Msk | PDMA_DSCT_CTL_DAINC_Msk);
+    PDMA->DSCT[channel].NEXT  = 0;
+
+    __IO uint32_t* reqsel     = &PDMA->REQSEL0_3;
+    uint32_t reg_offset       = channel / 4;
+    uint32_t bits_pos         = (channel % 4) * 8;
+
+    reqsel[reg_offset]        = (reqsel[reg_offset] & ~(PDMA_REQSEL0_3_REQSRC0_Msk << bits_pos)) | (peripheral_index << bits_pos) ;
+    __channals[channel].count = cfg_ptr->count;
+
+    PDMA->DSCT[channel].SA    = (uint32_t)cfg_ptr->src_address;
+    PDMA->DSCT[channel].DA    = (uint32_t)cfg_ptr->dst_address;
+    PDMA->DSCT[channel].CTL   = (PDMA->DSCT[channel].CTL & ~(PDMA_DSCT_CTL_TXCNT_Msk)) | ((cfg_ptr->count - 1) << PDMA_DSCT_CTL_TXCNT_Pos);
+
+    PDMA->CHCTL |= 1 << channel;        // enabled current channel
+
+    if (cfg_ptr->isr.handler_fn != NULL) {
+        __channals[channel].isr = cfg_ptr->isr;
+    }
+
+    return VSF_ERR_NONE;
+}
+
+bool m484_dma_channel_is_done(uint8_t channel)
+{
+    uint32_t tdstd = PDMA->TDSTS & (1 << channel);
+    PDMA->TDSTS = tdstd;
+
+    return !!tdstd;
+}
+
+vsf_err_t m484_dma_channel_irq_enable(uint8_t channel, uint32_t irq_mask)
+{
+    if (irq_mask & M484_DMA_TRANSFER_DONE_INTERRUPT) {
+        PDMA->INTEN |= 1 << channel;
+        __channals[channel].irq_mask |= M484_DMA_TRANSFER_DONE_INTERRUPT;
+    }
+    return VSF_ERR_NONE;
+}
+
+vsf_err_t m484_dma_channel_irq_disable(uint8_t channel, uint32_t irq_mask)
+{
+    if (irq_mask & M484_DMA_TRANSFER_DONE_INTERRUPT) {
+        PDMA->INTEN &= ~(1 << channel);
+        __channals[channel].irq_mask &= ~M484_DMA_TRANSFER_DONE_INTERRUPT;
+    }
+    return VSF_ERR_NONE;
+}
+
+vsf_err_t m484_dma_cancel_transfer(int8_t channel)
+{
+    //TODO: add cancel support
+    return VSF_ERR_NOT_SUPPORT;
+}
+
+int_fast32_t m484_dma_get_transfered_count(int8_t channel)
+{
+    //TODO: test transfered count
+    uint32_t transfer_count = (PDMA->DSCT[channel].CTL & PDMA_DSCT_CTL_TXCNT_Msk) >> PDMA_DSCT_CTL_TXCNT_Pos;
+    return __channals[channel].count - transfer_count - 1;
+}
+
+void PDMA_IRQHandler(void)
+{
+    uint32_t intsts = PDMA->INTSTS;
+    if (intsts & PDMA_INTSTS_TDIF_Msk) {
+        uint32_t transfer_done = PDMA->TDSTS;
+        PDMA->TDSTS = transfer_done;
+
+        for (int i = 0; i < M484_CHANNAL_MAX; i++) {
+            if (transfer_done & (1 << i)) {
+                if (__channals[i].isr.handler_fn != NULL && (__channals[i].irq_mask & M484_DMA_TRANSFER_DONE_INTERRUPT)) {
+                    __channals[i].isr.handler_fn(__channals[i].isr.target_ptr, M484_DMA_TRANSFER_DONE_INTERRUPT);
+                }
+            }
+        }
+
+    }
+
+    // Todo: Add timeout
+
+    PDMA->INTSTS = intsts;
+}
---
+++ source/hal/driver/Nuvoton/M480/common/dma/dma.h
@@ -0,0 +1,166 @@
+/*****************************************************************************
+ *   Copyright(C)2009-2019 by VSF Team                                       *
+ *                                                                           *
+ *  Licensed under the Apache License, Version 2.0 (the "License");          *
+ *  you may not use this file except in compliance with the License.         *
+ *  You may obtain a copy of the License at                                  *
+ *                                                                           *
+ *     http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                           *
+ *  Unless required by applicable law or agreed to in writing, software      *
+ *  distributed under the License is distributed on an "AS IS" BASIS,        *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and      *
+ *  limitations under the License.                                           *
+ *                                                                           *
+ ****************************************************************************/
+
+/*============================ INCLUDES ======================================*/
+#ifndef __HAL_DRIVER_NUVOTON_M480_DMA_H__
+#define __HAL_DRIVER_NUVOTON_M480_DMA_H__
+
+/*============================ INCLUDES ======================================*/
+#include "hal/vsf_hal_cfg.h"
+#include "../../__device.h"
+/*============================ MACROS ========================================*/
+ #define M484_DMA_REQUEST_MAX_SIZE    65536
+
+/*============================ MACROFIED FUNCTIONS ===========================*/
+/*============================ TYPES =========================================*/
+typedef struct m484_dma_cfg_t {
+    vsf_arch_prio_t prio;
+} m484_dma_cfg_t;
+
+typedef enum m484_dma_transfer_mode_t {
+    M484_DMA_MEM                        = 0UL,
+    M484_DMA_USB_TX                     = 2UL,
+    M484_DMA_USB_RX                     = 3UL,
+    M484_DMA_UART0_TX                   = 4UL,
+    M484_DMA_UART0_RX                   = 5UL,
+    M484_DMA_UART1_TX                   = 6UL,
+    M484_DMA_UART1_RX                   = 7UL,
+    M484_DMA_UART2_TX                   = 8UL,
+    M484_DMA_UART2_RX                   = 9UL,
+    M484_DMA_UART3_TX                   = 10UL,
+    M484_DMA_UART3_RX                   = 11UL,
+    M484_DMA_UART4_TX                   = 12UL,
+    M484_DMA_UART4_RX                   = 13UL,
+    M484_DMA_UART5_TX                   = 14UL,
+    M484_DMA_UART5_RX                   = 15UL,
+    M484_DMA_USCI0_TX                   = 16UL,
+    M484_DMA_USCI0_RX                   = 17UL,
+    M484_DMA_USCI1_TX                   = 18UL,
+    M484_DMA_USCI1_RX                   = 19UL,
+    M484_DMA_QSPI0_TX                   = 20UL,
+    M484_DMA_QSPI0_RX                   = 21UL,
+    M484_DMA_SPI0_TX                    = 22UL,
+    M484_DMA_SPI0_RX                    = 23UL,
+    M484_DMA_SPI1_TX                    = 24UL,
+    M484_DMA_SPI1_RX                    = 25UL,
+    M484_DMA_SPI2_TX                    = 26UL,
+    M484_DMA_SPI2_RX                    = 27UL,
+    M484_DMA_SPI3_TX                    = 28UL,
+    M484_DMA_SPI3_RX                    = 29UL,
+    M484_DMA_QSPI1_TX                   = 30UL,
+    M484_DMA_QSPI1_RX                   = 31UL,
+    M484_DMA_EPWM0_P1_RX                = 32UL,
+    M484_DMA_EPWM0_P2_RX                = 33UL,
+    M484_DMA_EPWM0_P3_RX                = 34UL,
+    M484_DMA_EPWM1_P1_RX                = 35UL,
+    M484_DMA_EPWM1_P2_RX                = 36UL,
+    M484_DMA_EPWM1_P3_RX                = 37UL,
+    M484_DMA_I2C0_TX                    = 38UL,
+    M484_DMA_I2C0_RX                    = 39UL,
+    M484_DMA_I2C1_TX                    = 40UL,
+    M484_DMA_I2C1_RX                    = 41UL,
+    M484_DMA_I2C2_TX                    = 42UL,
+    M484_DMA_I2C2_RX                    = 43UL,
+    M484_DMA_I2S0_TX                    = 44UL,
+    M484_DMA_I2S0_RX                    = 45UL,
+    M484_DMA_TMR0                       = 46UL,
+    M484_DMA_TMR1                       = 47UL,
+    M484_DMA_TMR2                       = 48UL,
+    M484_DMA_TMR3                       = 49UL,
+    M484_DMA_EADC0_RX                   = 50UL,
+    M484_DMA_DAC0_TX                    = 51UL,
+    M484_DMA_DAC1_TX                    = 52UL,
+    M484_DMA_EPWM0_CH0_TX               = 53UL,
+    M484_DMA_EPWM0_CH1_TX               = 54UL,
+    M484_DMA_EPWM0_CH2_TX               = 55UL,
+    M484_DMA_EPWM0_CH3_TX               = 56UL,
+    M484_DMA_EPWM0_CH4_TX               = 57UL,
+    M484_DMA_EPWM0_CH5_TX               = 58UL,
+    M484_DMA_EPWM1_CH0_TX               = 59UL,
+    M484_DMA_EPWM1_CH1_TX               = 60UL,
+    M484_DMA_EPWM1_CH2_TX               = 61UL,
+    M484_DMA_EPWM1_CH3_TX               = 62UL,
+    M484_DMA_EPWM1_CH4_TX               = 63UL,
+    M484_DMA_EPWM1_CH5_TX               = 64UL,
+    M484_DMA_UART6_TX                   = 66UL,
+    M484_DMA_UART6_RX                   = 67UL,
+    M484_DMA_UART7_TX                   = 68UL,
+    M484_DMA_UART7_RX                   = 69UL,
+    M484_DMA_EADC1_RX                   = 70UL,
+
+    M484_DMA_TRANSFER_WIDTH_8_BIT       = 0 << PDMA_DSCT_CTL_TXWIDTH_Pos,
+    M484_DMA_TRANSFER_WIDTH_16_BIT      = 1 << PDMA_DSCT_CTL_TXWIDTH_Pos,
+    M484_DMA_TRANSFER_WIDTH_32_BIT      = 2 << PDMA_DSCT_CTL_TXWIDTH_Pos,
+
+    M484_DMA_TRANSFER_SOURCE_INC        = 0 << PDMA_DSCT_CTL_SAINC_Pos,
+    M484_DMA_TRANSFER_SOURCE_FIXED      = 3 << PDMA_DSCT_CTL_SAINC_Pos,
+
+    M484_DMA_TRANSFER_DESTINATION_INC   = 0 << PDMA_DSCT_CTL_DAINC_Pos,
+    M484_DMA_TRANSFER_DESTINATION_FIXED = 3 << PDMA_DSCT_CTL_DAINC_Pos,
+
+
+
+    // TODO: support brust mode; In M484, brust only work for MEM to MEM
+} m484_dma_transfer_mode_t;
+
+typedef enum m484_dma_irq_mask_t {
+    M484_DMA_TRANSFER_DONE_INTERRUPT    = 1 << 0,
+    /*
+    TODO: add talbe and timeout interrupt support
+    M484_DMA_TABLE_EMPTY_INTERRUPT      = 1 << 1,
+    M484_DMA_TIMEOUT_INTERRUPT          = 1 << 2,
+    */
+} m484_dma_irq_mask_t;
+
+typedef void m484_dma_isr_handler_t(void *target_ptr, uint32_t irq_mask);
+
+typedef struct m484_dma_isr_t {
+    m484_dma_isr_handler_t      *handler_fn;
+    void                        *target_ptr;
+} m484_dma_isr_t;
+
+typedef struct m484_dma_channel_cfg_t {
+    uint32_t                 mode;
+    m484_dma_isr_t           isr;
+    void                     *src_address;
+    void                     *dst_address;
+    uint32_t                 count;
+} m484_dma_channel_cfg_t;
+
+typedef struct m484_dma_channel_t {
+    uint16_t count;
+    m484_dma_isr_t isr;
+    m484_dma_irq_mask_t irq_mask;
+} m484_dma_channel_t;
+
+/*============================ GLOBAL VARIABLES ==============================*/
+/*============================ PROTOTYPES ====================================*/
+
+vsf_err_t m484_dma_init(m484_dma_cfg_t *cfg_ptr);
+
+vsf_err_t m484_dma_channel_config(uint8_t channel, m484_dma_channel_cfg_t *cfg_ptr);
+bool m484_dma_channel_is_done(uint8_t channel);
+vsf_err_t m484_dma_cancel_transfer(int8_t channel);
+int_fast32_t m484_dma_get_transfered_count(int8_t channel);
+
+vsf_err_t m484_dma_channel_irq_enable(uint8_t channel, uint32_t irq_mask);
+vsf_err_t m484_dma_channel_irq_disable(uint8_t channel, uint32_t irq_mask);
+
+
+
+
+#endif
