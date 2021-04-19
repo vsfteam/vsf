3ad8bb8e8e4bbaa454a793eced5252fd6ca67c67
SimonQian <SimonQian@SimonQian.com>
2021-04-20 01:43:01 +0800

    [component/lua] add port

---
+++ source/component/3rd-party/lua/port/lua_port.c
@@ -0,0 +1,44 @@
+/*****************************************************************************
+ *   Copyright(C)2009-2019 by VSF Team                                       *
+ *                                                                           *
+ *  Licensed under the Apache License, Version 2.0 (the "License");          *
+ *  you may not use this file except in compliance with the License.         *
+ *  You may obtain a copy of the License at                                  *
+ *                                                                           *
+ *     http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                           *
+ *  Unless required by applicable law or agreed to in writing, software      *
+ *  distributed under the License is distributed on an "AS IS" BASIS,        *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and      *
+ *  limitations under the License.                                           *
+ *                                                                           *
+ ****************************************************************************/
+
+/*============================ INCLUDES ======================================*/
+
+#include "component/vsf_component_cfg.h"
+
+#if VSF_USE_LUA == ENABLED
+
+#include "utilities/vsf_utilities.h"
+
+/*============================ MACROS ========================================*/
+/*============================ MACROFIED FUNCTIONS ===========================*/
+/*============================ TYPES =========================================*/
+/*============================ GLOBAL VARIABLES ==============================*/
+/*============================ LOCAL VARIABLES ===============================*/
+/*============================ PROTOTYPES ====================================*/
+/*============================ IMPLEMENTATION ================================*/
+
+#if     __IS_COMPILER_IAR__                                                     \
+    &&  !(VSF_USE_LINUX == ENABLED && VSF_LINUX_USE_SIMPLE_LIBC == ENABLED && VSF_LINUX_USE_SIMPLE_STDIO == ENABLED)
+// loslib need rename in stdio.h, which is not available in iar libc, implement a substitute
+int rename(const char *old_filename, const char *new_filename)
+{
+    VSF_ASSERT(false);
+    return -1;
+}
+#endif
+
+#endif
