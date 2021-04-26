98b496514bf1c7c56afc3d6c2091ed9eca6ff5fc
SimonQian <SimonQian@SimonQian.com>
2021-04-26 21:02:20 +0800

    [example/template/usbd_uac_demo] fix interval

--- example/template/demo/usbd_demo/usbd_demo_uac.c
+++ example/template/demo/usbd_demo/usbd_demo_uac.c
@@ -31,10 +31,10 @@
 #endif
 
 #if     USRAPP_CFG_USBD_SPEED == USB_SPEED_HIGH
-//! interval for high speed is number of micro-frames
-#   define __USRAPP_USBD_UAC_CFG_EP_INTERVAL    8
+//! interval(iso ep) for high speed is (2 exp (interval - 1)) micro-frames / 125 microseconds
+#   define __USRAPP_USBD_UAC_CFG_EP_INTERVAL    4
 #elif   USRAPP_CFG_USBD_SPEED == USB_SPEED_FULL
-//! interval for full speed is number of micro-frames
+//! interval(iso ep) for full speed is number of frames / millisecond
 #   define __USRAPP_USBD_UAC_CFG_EP_INTERVAL    1
 #else
 #   error TODO: add support to current USB speed
