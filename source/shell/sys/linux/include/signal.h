61f21c35b52663e7524240ae1d07557c59348e85
SimonQian <SimonQian@SimonQian.com>
2021-05-04 12:48:28 +0800

    [shell/linux] fix SIG_DFL and SIG_IGN

--- source/shell/sys/linux/include/signal.h
+++ source/shell/sys/linux/include/signal.h
@@ -77,8 +77,8 @@ typedef struct {
 #define SIG_UNBLOCK     1
 #define SIG_SETMASK     2
 
-#define SIG_DFL         0
-#define SIG_IGN         -1
+#define SIG_DFL         (sighandler_t)0
+#define SIG_IGN         (sighandler_t)-1
 
 static inline void sigemptyset(sigset_t *set)
 {
