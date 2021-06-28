569aa916f14df21992f2cdeb066c01bb82ceb281
Yongxiang Liang <tanekliang@gmail.com>
2021-06-28 18:45:28 +0800

    [cmake] mps2 add an500
    Signed-off-by: Yongxiang Liang <tanekliang@gmail.com>

---
+++ script/cmake/targets/arm/__cortex_m7.cmake
@@ -0,0 +1,22 @@
+if(NOT ARM_CMSIS_PATH)
+    set(ARM_CMSIS_PATH ${VSF_SRC_PATH}/utilities/compiler/arm/3rd-party/CMSIS)
+endif()
+
+list(APPEND VSF_TARGET_INCLUDE_DIRECTORIES
+    ${ARM_CMSIS_PATH}/CMSIS/Core/Include
+    ${ARM_CMSIS_PATH}/CMSIS/DSP/Include
+    ${ARM_CMSIS_PATH}/CMSIS/NN/Include
+)
+
+set(CMAKE_C_FLAGS 
+    "-mcpu=cortex-m7 -mthumb ${CMAKE_C_FLAGS}"
+    CACHE INTERNAL "C compiler common flags"
+)
+set(CMAKE_CXX_FLAGS 
+    "-mcpu=cortex-m7 -mthumb ${CMAKE_CXX_FLAGS}"
+    CACHE INTERNAL "C++ compiler common flags"
+)
+set(CMAKE_ASM_FLAGS
+    "-mthumb ${CMAKE_ASM_FLAGS}"
+    CACHE INTERNAL "asm compiler common flags"
+)
---
+++ script/cmake/targets/arm/mps2/an500.cmake
@@ -0,0 +1,15 @@
+set(VSF_HAL_CHIP_VENDOR     arm)
+set(VSF_HAL_CHIP_SERIES     mps2)
+set(VSF_HAL_CHIP_NAME       CMSDK_CM7)
+set(VSF_ARCH_SERIES         arm)
+set(VSF_ARCH_NAME           cortex-m7)
+
+set(VSF_TARGET_DEFINITIONS
+    "__ARM__"
+    "__MPS2__"
+    "CMSDK_CM7"
+
+    ${VSF_TARGET_DEFINITIONS}
+)
+
+include(${VSF_CMAKE_ROOT}/targets/arm/__cortex_m7.cmake)
