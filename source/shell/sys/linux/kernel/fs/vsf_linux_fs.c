ff442996996f01fa625c3f40f9fb160aaf2e4035
vsfos <vsfos@foxmail.com>
2021-10-31 15:31:40 +0800

    [shell/linux] fix compile error if __OOC_DEBUG__(__OOC_CPP__) is not defined

--- source/shell/sys/linux/kernel/fs/vsf_linux_fs.c
+++ source/shell/sys/linux/kernel/fs/vsf_linux_fs.c
@@ -24,7 +24,7 @@
 #define __VSF_SIMPLE_STREAM_CLASS_INHERIT__
 #define __VSF_FS_CLASS_INHERIT__
 #define __VSF_LINUX_FS_CLASS_IMPLEMENT
-#define __VSF_LINUX_CLASS_INHERIT__
+#define __VSF_LINUX_CLASS_IMPLEMENT
 
 #if VSF_LINUX_CFG_RELATIVE_PATH == ENABLED
 #   include "../include/unistd.h"
