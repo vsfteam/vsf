c3937d63e5356fc1af3d2622b405f1f96f7a0dca
Yongxiang Liang <tanekliang@gmail.com>
2021-11-26 16:26:15 +0800

    [hal/template] disable multiple instances default

--- source/hal/driver/common/template/vsf_template_io.h
+++ source/hal/driver/common/template/vsf_template_io.h
@@ -27,7 +27,7 @@ extern "C" {
 /*============================ MACROS ========================================*/
 
 #ifndef VSF_HAL_GPIO_CFG_MULTI_INSTANCES
-#   define VSF_HAL_GPIO_CFG_MULTI_INSTANCES ENABLED
+#   define VSF_HAL_GPIO_CFG_MULTI_INSTANCES DISABLED
 #endif
 
 /*============================ MACROFIED FUNCTIONS ===========================*/
