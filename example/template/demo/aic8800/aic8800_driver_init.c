e46be06dcc9cc8841e4185537ee7e95f49ba92d2
Yongxiang Liang <tanekliang@gmail.com>
2021-12-29 19:31:43 +0800

    [example/aic8800] add io configuration

--- example/template/demo/aic8800/aic8800_driver_init.c
+++ example/template/demo/aic8800/aic8800_driver_init.c
@@ -17,86 +17,31 @@
 
 /*============================ INCLUDES ======================================*/
 
-#include "hal/vsf_hal_cfg.h"
-#include "./device.h"
-#include "./driver.h"
+#include "vsf.h"
 
-#include "time_api.h"
-#include "stdio_uart.h"
-#include "sysctrl_api.h"
+#ifdef __AIC8800__
 
-#include "dbg.h"
+#include "reg_iomux.h"
 
 /*============================ MACROS ========================================*/
 /*============================ MACROFIED FUNCTIONS ===========================*/
-
-#define __VSF_HW_RNG_INIT(__COUNT, __DONT_CARE)                                 \
-    vsf_hw_rng_init(&VSF_MCONNECT(vsf_rng, __COUNT));
-
 /*============================ TYPES =========================================*/
 /*============================ GLOBAL VARIABLES ==============================*/
-
-// to sustain the data in range [0x00100000, 0x0015FFFF] while enter low power mode,
-//  set __data_start__ and _shrbram to a value in the range
-int __data_start__ = 0x00100000;
-int _shrbram = 0x00100000;
-
-syscfg_predefined_t const syscfg_predefined = {
-    #if PLF_PMIC_LDO_VCORE
-    .pmic_vcore_dcdc_off = 1,
-    #elif PLF_PMIC_DCDC_VCORE
-    .pmic_vcore_ldo_off  = 1,
-    #endif
-    .pmic_vrtc09_ldo_off = 0,
-    #if PLF_LPO_512K
-    .pmic_lpo_512k_sel = 1,
-    #endif
-};
-
 /*============================ LOCAL VARIABLES ===============================*/
 /*============================ PROTOTYPES ====================================*/
 /*============================ IMPLEMENTATION ================================*/
 
-#include "hal/driver/common/swi/arm/vsf_swi_template.inc"
-
-#ifndef WEAK_VSF_DRIVER_AIC8800_INIT
-WEAK(vsf_driver_aic8800_init)
 bool vsf_driver_aic8800_init(void)
 {
+    iomux_gpio_config_sel_setf(10, 0x01); // PA10 as uart1 rx
+    iomux_gpio_config_sel_setf(11, 0x01); // PA11 as uart1 tx
+
+    iomux_gpio_config_sel_setf(6, 0x02); // PA10 as uart2 rx
+    iomux_gpio_config_sel_setf(7, 0x02); // PA11 as uart2 tx
+
     return true;
 }
-#endif
 
-int vsf_hal_pre_startup_init(void)
-{
-    // seems systick is not reset on hw reset of aic8800
-    // avoid systick interrupt before os started
-    SysTick->CTRL = 0;
-    return 1;
-}
-
-/*! \note initialize device driver
- *  \param none
- *  \retval true initialization succeeded.
- *  \retval false initialization failed
- */
-bool vsf_driver_init(void)
-{
-    SystemInit();
-    sysctrl_clock_cfg(CLK_CFG_D480S240P120F120);
-    SystemCoreClockUpdate();
-
-    stdio_uart_init();
-    dbg_init();
-
-    aic_time_init(0, 0);
-
-#if VSF_HAL_USE_RNG == ENABLED
-    VSF_MREPEAT(RNG_COUNT, __VSF_HW_RNG_INIT, NULL)
-#endif
-
-    return vsf_driver_aic8800_init();
-}
-
+#endif      // __AIC8800__
 
 /* EOF */
