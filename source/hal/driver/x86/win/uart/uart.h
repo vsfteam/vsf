ee94f8b04aa84255104f60826ef4399bbbe2f28a
Yongxiang Liang <tanekliang@gmail.com>
2021-12-30 17:16:02 +0800

    [hal/driver/usart] add rx timeout enum

--- source/hal/driver/x86/win/uart/uart.h
+++ source/hal/driver/x86/win/uart/uart.h
@@ -63,20 +63,22 @@ enum em_usart_mode_t {
     USART_HWCONTROL_MASK            =  USART_NO_HWCONTROL  | USART_RTS_HWCONTROL
                                      | USART_CTS_HWCONTROL | USART_RTS_CTS_HWCONTROL,
 
-    USART_TX_ENABLE                     = 0x0010ul,
-    USART_RX_ENABLE                     = 0x0020ul,
-    USART_ENABLE_MASK                   = USART_TX_ENABLE | USART_RX_ENABLE,
+    USART_TX_ENABLE                 = 0x0010ul,
+    USART_RX_ENABLE                 = 0x0020ul,
+    USART_ENABLE_MASK               = USART_TX_ENABLE | USART_RX_ENABLE,
 };
 
 enum em_usart_irq_mask_t {
     USART_IRQ_MASK_RX               = BIT(0),
     USART_IRQ_MASK_TX               = BIT(1),
-    USART_IRQ_MASK_RX_CPL           = BIT(2),
-    USART_IRQ_MASK_TX_CPL           = BIT(3),
+    USART_IRQ_MASK_RX_TIMEOUT       = BIT(2),
+
+    USART_IRQ_MASK_RX_CPL           = BIT(3),
+    USART_IRQ_MASK_TX_CPL           = BIT(4),
 
     // optional
-    USART_IRQ_MASK_RX_ERR           = BIT(4),
-    USART_IRQ_MASK_TX_ERR           = BIT(5),
+    USART_IRQ_MASK_RX_ERR           = BIT(5),
+    USART_IRQ_MASK_TX_ERR           = BIT(6),
     USART_IRQ_MASK_ERR              = USART_IRQ_MASK_RX_ERR | USART_IRQ_MASK_TX_ERR,
 
     USART_IRQ_MASK                  =   USART_IRQ_MASK_RX     | USART_IRQ_MASK_TX
