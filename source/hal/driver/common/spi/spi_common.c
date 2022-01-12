f9e7f5af82812ec58329a8a0e69aafe09b25d20e
Yongxiang Liang <tanekliang@gmail.com>
2022-01-12 17:04:00 +0800

    [hal/driver/spi] add spi template

---
+++ source/hal/driver/common/spi/spi_common.c
@@ -0,0 +1,159 @@
+/*****************************************************************************
+ *   Cop->right(C)2009-2019 by VSF Team                                       *
+ *                                                                           *
+ *  Licensed under the Apache License, Version 2.0 (the "License");          *
+ *  you may not use this file except in compliance with the License.         *
+ *  You may obtain a cop-> of the License at                                  *
+ *                                                                           *
+ *     http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                           *
+ *  Unless required by applicable law or agreed to in writing, software      *
+ *  distributed under the License is distributed on an "AS IS" BASIS,        *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and      *
+ *  limitations under the License.                                           *
+ *                                                                           *
+ ****************************************************************************/
+
+
+/*============================ INCLUDES ======================================*/
+
+#include "hal/driver/driver.h"
+
+#if VSF_HAL_USE_SPI == ENABLED
+
+/*============================ MACROS ========================================*/
+/*============================ MACROFIED FUNCTIONS ===========================*/
+/*============================ TYPES =========================================*/
+/*============================ GLOBAL VARIABLES ==============================*/
+/*============================ LOCAL VARIABLES ===============================*/
+/*============================ PROTOTYPES ====================================*/
+
+/*============================ IMPLEMENTATION ================================*/
+
+#if VSF_SPI_CFG_MULTI_INSTANCES == ENABLED
+
+vsf_err_t vsf_spi_init(vsf_spi_t *spi_ptr, spi_cfg_t *cfg_ptr)
+{
+    VSF_HAL_ASSERT(spi_ptr != NULL);
+    VSF_HAL_ASSERT(spi_ptr->op != NULL);
+    VSF_HAL_ASSERT(spi_ptr->op->init != NULL);
+
+    return spi_ptr->op->init(spi_ptr, cfg_ptr);
+}
+
+fsm_rt_t vsf_spi_enable(vsf_spi_t *spi_ptr)
+{
+    VSF_HAL_ASSERT(spi_ptr != NULL);
+    VSF_HAL_ASSERT(spi_ptr->op != NULL);
+    VSF_HAL_ASSERT(spi_ptr->op->enable != NULL);
+
+    return spi_ptr->op->enable(spi_ptr);
+}
+
+fsm_rt_t vsf_spi_disable(vsf_spi_t *spi_ptr)
+{
+    VSF_HAL_ASSERT(spi_ptr != NULL);
+    VSF_HAL_ASSERT(spi_ptr->op != NULL);
+    VSF_HAL_ASSERT(spi_ptr->op->disable != NULL);
+
+    return spi_ptr->op->disable(spi_ptr);
+}
+
+void vsf_spi_irq_enable(vsf_spi_t *spi_ptr, em_spi_irq_mask_t irq_mask)
+{
+    VSF_HAL_ASSERT(spi_ptr != NULL);
+    VSF_HAL_ASSERT(spi_ptr->op != NULL);
+    VSF_HAL_ASSERT(spi_ptr->op->irq_enable != NULL);
+
+    spi_ptr->op->irq_enable(spi_ptr, irq_mask);
+}
+
+void vsf_spi_irq_disable(vsf_spi_t *spi_ptr, em_spi_irq_mask_t irq_mask)
+{
+    VSF_HAL_ASSERT(spi_ptr != NULL);
+    VSF_HAL_ASSERT(spi_ptr->op != NULL);
+    VSF_HAL_ASSERT(spi_ptr->op->irq_disable != NULL);
+
+    spi_ptr->op->irq_disable(spi_ptr, irq_mask);
+}
+
+spi_status_t vsf_spi_status(vsf_spi_t *spi_ptr)
+{
+    VSF_HAL_ASSERT(spi_ptr != NULL);
+    VSF_HAL_ASSERT(spi_ptr->op != NULL);
+    VSF_HAL_ASSERT(spi_ptr->op->status != NULL);
+
+    return spi_ptr->op->status(spi_ptr);
+}
+
+void vsf_spi_cs_active(vsf_spi_t *spi_ptr, uint_fast8_t index)
+{
+    VSF_HAL_ASSERT(spi_ptr != NULL);
+    VSF_HAL_ASSERT(spi_ptr->op != NULL);
+    VSF_HAL_ASSERT(spi_ptr->op->cs_active != NULL);
+
+    spi_ptr->op->cs_active(spi_ptr, index);
+}
+
+void vsf_spi_cs_inactive(vsf_spi_t *spi_ptr, uint_fast8_t index)
+{
+    VSF_HAL_ASSERT(spi_ptr != NULL);
+    VSF_HAL_ASSERT(spi_ptr->op != NULL);
+    VSF_HAL_ASSERT(spi_ptr->op->cs_inactive != NULL);
+
+    spi_ptr->op->cs_inactive(spi_ptr, index);
+}
+
+void vsf_spi_fifo_transfer(vsf_spi_t *spi_ptr,
+                           void *out_buffer_ptr, uint_fast32_t *out_count_ptr,
+                           void *in_buffer_ptr, uint_fast32_t *in_count_ptr)
+{
+    VSF_HAL_ASSERT(spi_ptr != NULL);
+    VSF_HAL_ASSERT(spi_ptr->op != NULL);
+    VSF_HAL_ASSERT(spi_ptr->op->fifo_transfer != NULL);
+
+    spi_ptr->op->fifo_transfer(spi_ptr, out_buffer_ptr, out_count_ptr,
+                                        in_buffer_ptr,  in_count_ptr);
+}
+
+bool vsf_spi_fifo_flush(vsf_spi_t *spi_ptr)
+{
+    VSF_HAL_ASSERT(spi_ptr != NULL);
+    VSF_HAL_ASSERT(spi_ptr->op != NULL);
+    VSF_HAL_ASSERT(spi_ptr->op->fifo_flush != NULL);
+
+    return spi_ptr->op->fifo_flush(spi_ptr);
+}
+
+vsf_err_t vsf_spi_request_transfer(vsf_spi_t *spi_ptr, void *out_buffer_ptr,
+                                   void *in_buffer_ptr, uint_fast32_t count)
+{
+    VSF_HAL_ASSERT(spi_ptr != NULL);
+    VSF_HAL_ASSERT(spi_ptr->op != NULL);
+    VSF_HAL_ASSERT(spi_ptr->op->request_transfer != NULL);
+
+    return spi_ptr->op->request_transfer(spi_ptr, out_buffer_ptr,
+                                         in_buffer_ptr, count);
+}
+
+vsf_err_t vsf_spi_cancel_transfer(vsf_spi_t *spi_ptr)
+{
+    VSF_HAL_ASSERT(spi_ptr != NULL);
+    VSF_HAL_ASSERT(spi_ptr->op != NULL);
+    VSF_HAL_ASSERT(spi_ptr->op->cancel_transfer != NULL);
+
+    return spi_ptr->op->cancel_transfer(spi_ptr);
+}
+
+int_fast32_t vsf_spi_get_transfered_count(vsf_spi_t *spi_ptr)
+{
+    VSF_HAL_ASSERT(spi_ptr != NULL);
+    VSF_HAL_ASSERT(spi_ptr->op != NULL);
+    VSF_HAL_ASSERT(spi_ptr->op->get_transfered_count != NULL);
+
+    return spi_ptr->op->get_transfered_count(spi_ptr);
+}
+
+#endif /* VSF_SPI_CFG_MULTI_INSTANCES == ENABLED */
+#endif /* VSF_HAL_USE_SPI == ENABLED */
