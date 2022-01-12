2004b560c1bf06b9a17405a1809559dc4fa96d62
Yongxiang Liang <tanekliang@gmail.com>
2022-01-12 17:03:05 +0800

    [hal/driver/aic8800] fixed spi build error

--- source/hal/driver/AIC/AIC8800/spi/spi.h
+++ source/hal/driver/AIC/AIC8800/spi/spi.h
@@ -27,65 +27,56 @@
 #include "./hal/driver/AIC/AIC8800/vendor/plf/aic8800/src/driver/iomux/reg_iomux.h"
 
 /*============================ MACROS ========================================*/
+
+#define VSF_SPI_REIMPLEMENT_MODE                ENABLED
+
 /*============================ MACROFIED FUNCTIONS ===========================*/
 /*============================ TYPES =========================================*/
 
-typedef enum em_spi_irq_mask_t {
-    SPI_IRQ_MASK_TX_CPL = BIT(0),
-    SPI_IRQ_MASK_CPL    = BIT(1),
-} em_spi_irq_mask_t;
-
 typedef enum em_spi_mode_t {
-    SPI_SLAVE                               = 0x00,
-    SPI_MASTER                              = BIT(0),
+    SPI_SLAVE                        = 0x00ul << 0,
+    SPI_MASTER                       = 0x01ul << 0,
+    SPI_DIR_MODE_MASK                = SPI_SLAVE | SPI_MASTER,
 
-    SPI_MODE_0                              = 0,
-    SPI_MODE_1                              = BIT(1),
-    SPI_MODE_2                              = BIT(2),
-    SPI_MODE_3                              = SPI_MODE_1 | SPI_MODE_2,
+    SPI_MODE_0                       = 0x00ul << 1,
+    SPI_MODE_1                       = 0x01ul << 1,
+    SPI_MODE_2                       = 0x02ul << 1,
+    SPI_MODE_3                       = 0x02ul << 1,
+    SPI_CLOCK_MODE_MASK              =  SPI_MODE_0 | SPI_MODE_1
+                                      | SPI_MODE_2 | SPI_MODE_3,
 
-    SPI_DATASIZE_8                          = 0, //todo
+    SPI_MSB_FIRST                    = 0x00ul << 3,
+    SPI_LSB_FIRST                    = 0x01ul << 3,
+    SPI_BIT_ORDER_MASK               = SPI_MSB_FIRST | SPI_LSB_FIRST,
 
-    SPI_MSB_FIRST                           = 0,
-    SPI_LSB_FIRST                           = BIT(3),
+    SPI_AUTO_CS_DISABLE              = 0x00ul << 4,
+    SPI_AUTO_CS_ENABLE               = 0x01ul << 4,
+    SPI_AUTO_CS_MASK                 = SPI_AUTO_CS_DISABLE | SPI_AUTO_CS_ENABLE,
 
-    SPI_AUTO_SLAVE_SELECTION_DISABLE        = 0,
-    SPI_AUTO_SLAVE_SELECTION_ENABLE         = BIT(4),
+    //SPI_SLAVE_SELECTION_ACTIVE_LOW   = 0,
+    //SPI_SLAVE_SELECTION_ACTIVE_HIGH  = BIT(5),
 
-    SPI_SLAVE_SELECTION_ACTIVE_LOW          = 0,
-    SPI_SLAVE_SELECTION_ACTIVE_HIGH         = BIT(5),
+    __SPI_HW_MODE_MASK               =  SPI_DIR_MODE_MASK
+                                      | SPI_CLOCK_MODE_MASK
+                                      | SPI_BIT_ORDER_MASK
+                                      | SPI_AUTO_CS_MASK,
+
+    SPI_DATASIZE_8                   = 0x00ul << 8, //todo
+    SPI_DATASIZE_MASK                = 0x1Ful << 8,
+
+    SPI_MODE_MASK                    = __SPI_HW_MODE_MASK | SPI_DATASIZE_MASK,
 } em_spi_mode_t;
+
+
+typedef struct vsf_hw_spi_t vsf_hw_spi_t;
+
 /*============================ INCLUDES ======================================*/
 
 #include "hal/driver/common/template/vsf_template_spi.h"
 
-struct spi_status_t {
-    union {
-        inherit(peripheral_status_t)
-        uint32_t                value;
-    };
-};
-
-typedef struct vsf_spi_t {
-    REG_SPI_T                   *param;
-    spi_cfg_t                   cfg;
-    em_spi_irq_mask_t           irq_msk;
-    spi_status_t                status;
-    struct {
-        uint32_t                count;
-        uint32_t                off_set_64;
-        struct {
-            void                *out_buff;
-            void                *in_buff;
-        } buff;
-    } __off_set;
-    bool                        is_able;
-    bool                        is_busy;
-} vsf_spi_t;
-
 /*============================ GLOBAL VARIABLES ==============================*/
 
-extern vsf_spi_t vsf_spi0;
+extern vsf_hw_spi_t vsf_spi0;
 
 /*============================ LOCAL VARIABLES ===============================*/
 /*============================ PROTOTYPES ====================================*/
