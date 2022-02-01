b3aee7039761513568bf766df458747e54d452f4
vsfos <vsfos@foxmail.com>
2022-02-01 20:18:10 +0800

    [shell/linux] add ioctl, redirected to fcntl

---
+++ source/shell/sys/linux/include/sys/ioctl.h
@@ -0,0 +1,25 @@
+#ifndef __VSF_LINUX_IOCTL_H__
+#define __VSF_LINUX_IOCTL_H__
+
+#include "shell/sys/linux/vsf_linux_cfg.h"
+
+#if VSF_LINUX_CFG_RELATIVE_PATH == ENABLED
+#   include "../sys/types.h"
+#   include "../fcntl.h"
+#else
+#   include <sys/types.h>
+#   include <fcntl.h>
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//int ioctl(int fd, unsigned long request, ...);
+#define ioctl       fcntl
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif      // __VSF_LINUX_IOCTL_H__
