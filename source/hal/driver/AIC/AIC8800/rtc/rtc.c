233d9b9ff05530030c01088a274496812fcb4c89
liangyongxiang <tanekliang@gmail.com>
2022-05-23 15:56:03 +0800

    [hal/driver/aic8800] update rtc

--- source/hal/driver/AIC/AIC8800/rtc/rtc.c
+++ source/hal/driver/AIC/AIC8800/rtc/rtc.c
@@ -66,15 +66,43 @@ fsm_rt_t vsf_hw_rtc_disable(vsf_hw_rtc_t *hw_rtc_ptr)
     return fsm_rt_cpl;
 }
 
+vsf_err_t vsf_hw_rtc_get_time(vsf_hw_rtc_t *hw_rtc_ptr, time_t *second, time_t *milliseconds)
+{
+    uint32_t sec;
+    uint32_t usec;
+    VSF_HAL_ASSERT(second == 0);
+
+    int result = aic_time_get(SINCE_EPOCH, &sec, &usec);
+    VSF_HAL_ASSERT(result == 0);
+    (void) result;
+
+    *second = sec;
+    if (milliseconds != NULL) {
+        *milliseconds = usec / 1000;
+    }
+
+    return VSF_ERR_NONE;
+}
+
+vsf_err_t vsf_hw_rtc_set_time(vsf_hw_rtc_t *hw_rtc_ptr, time_t time, time_t milliseconds)
+{
+    aic_time_update((uint32_t)time, milliseconds * 1000);
+    return VSF_ERR_NONE;
+}
+
 vsf_err_t vsf_hw_rtc_get(vsf_hw_rtc_t *hw_rtc_ptr, vsf_rtc_tm_t *rtc_tm)
 {
     VSF_HAL_ASSERT(NULL != hw_rtc_ptr);
     VSF_HAL_ASSERT(NULL != rtc_tm);
 
-    time_t time = vsf_hw_rtc_get_second(hw_rtc_ptr);
+    time_t second, milliseconds;
+    vsf_err_t result = vsf_hw_rtc_get_time(hw_rtc_ptr, &second, &milliseconds);
+    if (result != VSF_ERR_NONE) {
+        return result;
+    }
 
     struct tm tm_local;
-    gmtime_offset_r(&time, &tm_local, 0);
+    gmtime_offset_r(&second, &tm_local, 0);
 
     rtc_tm->tm_sec  = tm_local.tm_sec;          // [0 .. 59]
     rtc_tm->tm_min  = tm_local.tm_min;          // [0 .. 59]
@@ -83,6 +111,7 @@ vsf_err_t vsf_hw_rtc_get(vsf_hw_rtc_t *hw_rtc_ptr, vsf_rtc_tm_t *rtc_tm)
     rtc_tm->tm_wday = tm_local.tm_wday + 1;     // [1 .. 7]   - [Sunday, Monday, ..., Saturday]
     rtc_tm->tm_mon  = tm_local.tm_mon;          // [1 .. 12]  - [January -- December]
     rtc_tm->tm_year = tm_local.tm_year + 1900;  // [1900 .. ]
+    rtc_tm->tm_ms   = milliseconds;
 
     return VSF_ERR_NONE;
 }
@@ -102,25 +131,7 @@ vsf_err_t vsf_hw_rtc_set(vsf_hw_rtc_t *hw_rtc_ptr, const vsf_rtc_tm_t *rtc_tm)
     tm_local.tm_year  = rtc_tm->tm_year - 1900; // [1900 .. ]
     time_t time = mk_gmtime_offset_r(&tm_local, 0);
 
-    return vsf_hw_rtc_set_second(hw_rtc_ptr, time);
-}
-
-time_t vsf_hw_rtc_get_second(vsf_hw_rtc_t *hw_rtc_ptr)
-{
-    uint32_t sec;
-    uint32_t usec;
-
-    int result = aic_time_get(SINCE_EPOCH, &sec, &usec);
-    VSF_HAL_ASSERT(result == 0);
-    (void) result;
-
-    return (time_t)sec;
-}
-
-vsf_err_t vsf_hw_rtc_set_second(vsf_hw_rtc_t *hw_rtc_ptr, time_t time)
-{
-    aic_time_update((uint32_t)time, 0);
-    return VSF_ERR_NONE;
+    return vsf_hw_rtc_set_time(hw_rtc_ptr, time, rtc_tm->tm_ms);
 }
 
 /*============================ MACROFIED FUNCTIONS ===========================*/
