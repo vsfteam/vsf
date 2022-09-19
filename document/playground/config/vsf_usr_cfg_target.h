4036583d3eff55d07b84fbc56315e6fc2fdd60df
vsfos <vsfos@foxmail.com>
2022-09-20 01:19:34 +0800

    [document/playground] update

--- document/playground/config/vsf_usr_cfg_target.h
+++ document/playground/config/vsf_usr_cfg_target.h
@@ -15,25 +15,25 @@
  *                                                                           *
  ****************************************************************************/
 
-#ifndef __VSF_USR_CFG_ARCH_H__
-#define __VSF_USR_CFG_ARCH_H__
+#ifndef __VSF_USR_CFG_TARGET_H__
+#define __VSF_USR_CFG_TARGET_H__
 
 /*============================ INCLUDES ======================================*/
 
 #if     defined(__M484__)
-#   include "./arch/vsf_usr_cfg_m484.h"
+#   include "./target/vsf_usr_cfg_m484.h"
 #elif   defined(__F1C100S__)
-#   include "./arch/vsf_usr_cfg_f1c100s.h"
+#   include "./target/vsf_usr_cfg_f1c100s.h"
 #elif   defined(__WIN__)
-#   include "./arch/vsf_usr_cfg_win.h"
+#   include "./target/vsf_usr_cfg_win.h"
 #elif   defined(__LINUX__)
-#   include "./arch/vsf_usr_cfg_linux.h"
+#   include "./target/vsf_usr_cfg_linux.h"
 #elif   defined(__GD32E103__)
-#   include "./arch/vsf_usr_cfg_gd32e103.h"
+#   include "./target/vsf_usr_cfg_gd32e103.h"
 #elif   defined(__MPS2__)
-#   include "./arch/vsf_usr_cfg_mps2.h"
+#   include "./target/vsf_usr_cfg_mps2.h"
 #elif   defined(__AIC8800__)
-#   include "./arch/vsf_usr_cfg_aic8800.h"
+#   include "./target/vsf_usr_cfg_aic8800.h"
 #endif
 
 /*============================ MACROS ========================================*/
