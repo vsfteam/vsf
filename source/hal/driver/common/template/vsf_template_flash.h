2fbb455535167bbee5125f6c1f4240513245f2cc
Yongxiang Liang <tanekliang@gmail.com>
2022-09-27 14:26:16 +0800

    [hal/driver/common] update flash template

--- source/hal/driver/common/template/vsf_template_flash.h
+++ source/hal/driver/common/template/vsf_template_flash.h
@@ -58,14 +58,18 @@ extern "C" {
 
 /*============================ MACROFIED FUNCTIONS ===========================*/
 
-#define VSF_FLASH_APIS(__prefix_name)                                                                                                                                                \
-    __VSF_HAL_TEMPLATE_API(__prefix_name, vsf_err_t,              flash, init,      VSF_MCONNECT(__prefix_name, _flash_t) *flash_ptr, vsf_flash_cfg_t *cfg_ptr)                                      \
-    __VSF_HAL_TEMPLATE_API(__prefix_name, fsm_rt_t,               flash, enable,    VSF_MCONNECT(__prefix_name, _flash_t) *flash_ptr)                                                            \
-    __VSF_HAL_TEMPLATE_API(__prefix_name, fsm_rt_t,               flash, disable,   VSF_MCONNECT(__prefix_name, _flash_t) *flash_ptr)                                                            \
-    __VSF_HAL_TEMPLATE_API(__prefix_name, vsf_flash_capability_t, flash, capability,VSF_MCONNECT(__prefix_name, _flash_t) *flash_ptr)                                                            \
-    __VSF_HAL_TEMPLATE_API(__prefix_name, vsf_err_t,              flash, erase,     VSF_MCONNECT(__prefix_name, _flash_t) *flash_ptr, vsf_flash_size_t offset, vsf_flash_size_t size)                  \
-    __VSF_HAL_TEMPLATE_API(__prefix_name, vsf_err_t,              flash, write,     VSF_MCONNECT(__prefix_name, _flash_t) *flash_ptr, vsf_flash_size_t offset, uint8_t* buffer, vsf_flash_size_t size) \
-    __VSF_HAL_TEMPLATE_API(__prefix_name, vsf_err_t,              flash, read,      VSF_MCONNECT(__prefix_name, _flash_t) *flash_ptr, vsf_flash_size_t offset, uint8_t* buffer, vsf_flash_size_t size)
+#define VSF_FLASH_APIS(__prefix_name) \
+    __VSF_HAL_TEMPLATE_API(__prefix_name, vsf_err_t,              flash, init,                  VSF_MCONNECT(__prefix_name, _flash_t) *flash_ptr, vsf_flash_cfg_t *cfg_ptr) \
+    __VSF_HAL_TEMPLATE_API(__prefix_name, fsm_rt_t,               flash, enable,                VSF_MCONNECT(__prefix_name, _flash_t) *flash_ptr) \
+    __VSF_HAL_TEMPLATE_API(__prefix_name, fsm_rt_t,               flash, disable,               VSF_MCONNECT(__prefix_name, _flash_t) *flash_ptr) \
+    __VSF_HAL_TEMPLATE_API(__prefix_name, vsf_flash_capability_t, flash, capability,            VSF_MCONNECT(__prefix_name, _flash_t) *flash_ptr) \
+    __VSF_HAL_TEMPLATE_API(__prefix_name, vsf_err_t,              flash, erase_one_sector,      VSF_MCONNECT(__prefix_name, _flash_t) *flash_ptr, vsf_flash_size_t offset_of_bytes) \
+    __VSF_HAL_TEMPLATE_API(__prefix_name, vsf_err_t,              flash, erase_multi_sector,    VSF_MCONNECT(__prefix_name, _flash_t) *flash_ptr, vsf_flash_size_t offset_of_bytes, vsf_flash_size_t size_of_bytes) \
+    __VSF_HAL_TEMPLATE_API(__prefix_name, vsf_err_t,              flash, erase_all,             VSF_MCONNECT(__prefix_name, _flash_t) *flash_ptr) \
+    __VSF_HAL_TEMPLATE_API(__prefix_name, vsf_err_t,              flash, write_one_sector,      VSF_MCONNECT(__prefix_name, _flash_t) *flash_ptr, vsf_flash_size_t offset_of_bytes, uint8_t* buffer, vsf_flash_size_t size_of_bytes) \
+    __VSF_HAL_TEMPLATE_API(__prefix_name, vsf_err_t,              flash, write_multi_sector,    VSF_MCONNECT(__prefix_name, _flash_t) *flash_ptr, vsf_flash_size_t offset_of_bytes, uint8_t* buffer, vsf_flash_size_t size_of_bytes) \
+    __VSF_HAL_TEMPLATE_API(__prefix_name, vsf_err_t,              flash, read_one_sector,       VSF_MCONNECT(__prefix_name, _flash_t) *flash_ptr, vsf_flash_size_t offset_of_bytes, uint8_t* buffer, vsf_flash_size_t size_of_bytes) \
+    __VSF_HAL_TEMPLATE_API(__prefix_name, vsf_err_t,              flash, read_multi_sector,     VSF_MCONNECT(__prefix_name, _flash_t) *flash_ptr, vsf_flash_size_t offset_of_bytes, uint8_t* buffer, vsf_flash_size_t size_of_bytes)
 
 /*============================ TYPES =========================================*/
 
@@ -111,6 +115,7 @@ typedef struct vsf_flash_capability_t {
     struct {
         // there are some flash only supports writing from the first address of the sector
         uint8_t can_write_any_address : 1;
+        uint8_t can_read_any_address  : 1;
     };
 } vsf_flash_capability_t;
 #endif
@@ -153,35 +158,56 @@ extern fsm_rt_t vsf_flash_enable(vsf_flash_t *flash_ptr);
 
 extern fsm_rt_t vsf_flash_disable(vsf_flash_t *flash_ptr);
 
+extern vsf_err_t vsf_flash_erase_one_sector(vsf_flash_t *flash_ptr,
+                                            vsf_flash_size_t offset_of_bytes);
+
 /**
  * flash erase a continuous range
- * @note offset must be aligend to the start address
- *       of the erase sector. size must be an integer
- *       multiple of the minimum erase size
+ * @note offset_of_bytes must be aligend to the start address
+ *       of the erase sector. size_of_bytes must be an integer
+ *       multiple of the minimum erase size_of_bytes
  *
  * @param[in] flash_ptr flash instance
- * @param[in] offset flash address offset
- * @param[in] size flash erase size(bytes)
+ * @param[in] offset_of_bytes flash address offset_of_bytes
+ * @param[in] size_of_bytes flash erase size_of_bytes(bytes)
  */
-extern vsf_err_t vsf_flash_erase(vsf_flash_t *flash_ptr,
-                                 vsf_flash_size_t offset,
-                                 vsf_flash_size_t size);
+extern vsf_err_t vsf_flash_erase_multi_sector(vsf_flash_t *flash_ptr,
+                                              vsf_flash_size_t offset_of_bytes,
+                                              vsf_flash_size_t size_of_bytes);
+
+/**
+ * flash erase all
+ *
+ * @param[in] flash_ptr flash instance
+ */
+extern vsf_err_t vsf_flash_erase_all(vsf_flash_t *flash_ptr);
+
+
+extern vsf_err_t vsf_flash_write_one_sector(vsf_flash_t *flash_ptr,
+                                            vsf_flash_size_t offset_of_bytes,
+                                            uint8_t* buffer,
+                                            vsf_flash_size_t size_of_bytes);
 
 /**
  * flash write a continuous range
- * @note offset must be aligend to the start address
- *       of the write sector. size must be an integer
- *       multiple of the minimum write size
+ * @note offset_of_bytes must be aligend to the start address
+ *       of the write sector. size_of_bytes must be an integer
+ *       multiple of the minimum write size_of_bytes
  *
  * @param[in] flash_ptr flash instance
- * @param[in] offset flash address offset
+ * @param[in] offset_of_bytes flash address offset_of_bytes
  * @param[out] buffer flash data to write
- * @param[in] size flash write size(bytes)
+ * @param[in] size_of_bytes flash write size_of_bytes(bytes)
  */
-extern vsf_err_t vsf_flash_write(vsf_flash_t *flash_ptr,
-                                 vsf_flash_size_t offset,
-                                 uint8_t* buffer,
-                                 vsf_flash_size_t size);
+extern vsf_err_t vsf_flash_write_multi_sector(vsf_flash_t *flash_ptr,
+                                              vsf_flash_size_t offset_of_bytes,
+                                              uint8_t* buffer,
+                                              vsf_flash_size_t size_of_bytes);
+
+extern vsf_err_t vsf_flash_read_one_sector(vsf_flash_t *flash_ptr,
+                                           vsf_flash_size_t offset_of_bytes,
+                                           uint8_t* buffer,
+                                           vsf_flash_size_t size_of_bytes);
 
 /**
  * flash read a continuous range
@@ -189,37 +215,44 @@ extern vsf_err_t vsf_flash_write(vsf_flash_t *flash_ptr,
  *       the unaligned range should be returned as a failure.
  *
  * @param[in] flash_ptr flash instance
- * @param[in] offset flash address offset
+ * @param[in] offset_of_bytes flash address offset_of_bytes
  * @param[in] buffer flash data to read
- * @param[in] size flash write size(bytes)
+ * @param[in] size_of_bytes flash write size_of_bytes(bytes)
  */
-extern vsf_err_t vsf_flash_read(vsf_flash_t *flash_ptr,
-                                vsf_flash_size_t offset,
-                                uint8_t* buffer,
-                                vsf_flash_size_t size);
-
-/** TODO:
- * information query API, include:
- * - minimum erase size
- * - minimum write size
- * - whether to allow random read
- */
+extern vsf_err_t vsf_flash_read_multi_sector(vsf_flash_t *flash_ptr,
+                                             vsf_flash_size_t offset_of_bytes,
+                                             uint8_t* buffer,
+                                             vsf_flash_size_t size_of_bytes);
 
 /*============================ MACROFIED FUNCTIONS ===========================*/
 
 #if VSF_FLASH_CFG_FUNCTION_RENAME == ENABLED
 #   define vsf_flash_init(__FLASH, ...)                                         \
-        VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_init)     ((VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_t) *)__FLASH, ##__VA_ARGS__)
+        VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_init)                 ((VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_t) *)__FLASH, ##__VA_ARGS__)
 #   define vsf_flash_enable(__FLASH)                                            \
-        VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_enable)   ((VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_t) *)__FLASH)
+        VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_enable)               ((VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_t) *)__FLASH)
 #   define vsf_flash_disable(__FLASH)                                           \
-        VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_disable)  ((VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_t) *)__FLASH)
+        VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_disable)              ((VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_t) *)__FLASH)
+#   define vsf_flash_erase_one_sector(__FLASH, ...)                             \
+        VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_erase_one_sector)     ((VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_t) *)__FLASH, ##__VA_ARGS__)
+#   define vsf_flash_erase_multi_sector(__FLASH, ...)                           \
+        VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_erase_multi_sector)   ((VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_t) *)__FLASH, ##__VA_ARGS__)
 #   define vsf_flash_erase(__FLASH, ...)                                        \
-        VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_erase)    ((VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_t) *)__FLASH, ##__VA_ARGS__)
+        VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_erase_multi_sector)   ((VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_t) *)__FLASH, ##__VA_ARGS__)
+#   define vsf_flash_erase_all(__FLASH, ...)                                    \
+        VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_erase_all)            ((VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_t) *)__FLASH, ##__VA_ARGS__)
+#   define vsf_flash_write_one_sector(__FLASH, ...)                             \
+        VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_write_one_sector)     ((VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_t) *)__FLASH, ##__VA_ARGS__)
+#   define vsf_flash_write_multi_sector(__FLASH, ...)                           \
+        VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_write_multi_sector)   ((VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_t) *)__FLASH, ##__VA_ARGS__)
 #   define vsf_flash_write(__FLASH, ...)                                        \
-        VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_write)    ((VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_t) *)__FLASH, ##__VA_ARGS__)
+        VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_write_multi_sector)   ((VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_t) *)__FLASH, ##__VA_ARGS__)
+#   define vsf_flash_read_one_sector(__FLASH, ...)                              \
+        VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_read_one_sector)      ((VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_t) *)__FLASH, ##__VA_ARGS__)
+#   define vsf_flash_read_multi_sector(__FLASH, ...)                            \
+        VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_read_multi_sector)    ((VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_t) *)__FLASH, ##__VA_ARGS__)
 #   define vsf_flash_read(__FLASH, ...)                                         \
-        VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_read)     ((VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_t) *)__FLASH, ##__VA_ARGS__)
+        VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_read_multi_sector)    ((VSF_MCONNECT(VSF_FLASH_CFG_PREFIX, _flash_t) *)__FLASH, ##__VA_ARGS__)
 #endif
 
 #ifdef __cplusplus
