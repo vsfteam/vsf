cb30662ddadc173b57ca8d06a409c3fcdedb04b3
vsfos <vsfos@foxmail.com>
2022-10-17 12:12:59 +0800

    [kernel] fix vsf_eda_mutex_leave_isr

--- source/kernel/vsf_eda.h
+++ source/kernel/vsf_eda.h
@@ -97,7 +97,7 @@ extern "C" {
 #define vsf_eda_mutex_leave(__pmtx)                                             \
             vsf_eda_sync_increase(&((__pmtx)->use_as__vsf_sync_t))
 #if VSF_SYNC_CFG_SUPPORT_ISR == ENABLED
-#   define vsf_eda_mutex_leave_isr(__pmtx)  vsf_eda_sync_increase_isr((__pmtx))
+#   define vsf_eda_mutex_leave_isr(__pmtx)  vsf_eda_sync_increase_isr(&(__pmtx)->use_as__vsf_sync_t)
 #endif
 
 // CRIT
