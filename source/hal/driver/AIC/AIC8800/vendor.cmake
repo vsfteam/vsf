4f52d8cad332b88ac48d10eafa49e152194ac8da
vsfos <vsfos@foxmail.com>
2022-10-30 14:58:24 +0800

    [hal/aic8800/cmake] check VSF_USE_LWIP and VSF_USE_LWIP_VENDOR while including lwip and wifi

--- source/hal/driver/AIC/AIC8800/vendor.cmake
+++ source/hal/driver/AIC/AIC8800/vendor.cmake
@@ -1,20 +1,76 @@
 # CMakeLists head
 
-target_compile_definitions(${VSF_TARGET_NAME} PUBLIC
-    # necessary for llvm to avoid compiler errors: redefinition u32_t etc.
-    LWIP_NO_STDINT_H=1
-)
-target_include_directories(${VSF_TARGET_NAME} PUBLIC
-    vendor/lwip/net_al
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/ports/rtos/include
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/include
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/include/lwip
+if(VSF_USE_LWIP AND VSF_USE_LWIP_VENDOR)
+    target_compile_definitions(${VSF_TARGET_NAME} PUBLIC
+        # necessary for llvm to avoid compiler errors: redefinition u32_t etc.
+        LWIP_NO_STDINT_H=1
+    )
+    target_include_directories(${VSF_TARGET_NAME} PUBLIC
+        vendor/lwip/net_al
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/ports/rtos/include
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/include
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/include/lwip
 
-    vendor/wifi/fhost
-    vendor/wifi/fhost/application
-    vendor/wifi/wlan
-    vendor/wifi/macif
-)
+        vendor/wifi/fhost
+        vendor/wifi/fhost/application
+        vendor/wifi/wlan
+        vendor/wifi/macif
+    )
+    target_sources(${VSF_LIB_NAME} INTERFACE
+        vendor/lwip/dhcps/dhcps.c
+        vendor/lwip/net_al/net_al.c
+        vendor/lwip/net_al/net_iperf_al.c
+        vendor/lwip/net_al/net_tg_al.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/ports/rtos/sys_arch.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/apps/mdns/mdns.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/api/api_lib.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/api/api_msg.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/api/err.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/api/netbuf.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/api/netdb.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/api/netifapi.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/api/sockets.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/api/tcpip.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv4/autoip.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv4/dhcp.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv4/etharp.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv4/icmp.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv4/igmp.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv4/ip4.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv4/ip4_addr.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv4/ip4_frag.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv6/dhcp6.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv6/ethip6.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv6/icmp6.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv6/inet6.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv6/ip6.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv6/ip6_addr.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv6/ip6_frag.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv6/mld6.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv6/nd6.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/def.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/dns.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/inet_chksum.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/init.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ip.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/mem.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/memp.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/netif.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/pbuf.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/raw.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/stats.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/sys.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/tcp.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/tcp_in.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/tcp_out.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/timeouts.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/udp.c
+        vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/netif/ethernet.c
+
+        vendor/wifi/fhost/fhost_config.c
+        vendor/wifi/macif/rom_patch.c
+    )
+endif()
 
 get_filename_component(AIC8800_LIBDRV vendor/plf/aic8800/lib/armgcc_4_8/libdrv_lite.a ABSOLUTE)
 get_filename_component(AIC8800_LIBWIFI vendor/wifi/lib/armgcc_4_8/libwifi.a ABSOLUTE)
@@ -26,56 +82,6 @@ target_link_libraries(${VSF_LIB_NAME} INTERFACE
 )
 
 target_sources(${VSF_LIB_NAME} INTERFACE
-    vendor/lwip/dhcps/dhcps.c
-    vendor/lwip/net_al/net_al.c
-    vendor/lwip/net_al/net_iperf_al.c
-    vendor/lwip/net_al/net_tg_al.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/ports/rtos/sys_arch.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/apps/mdns/mdns.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/api/api_lib.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/api/api_msg.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/api/err.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/api/netbuf.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/api/netdb.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/api/netifapi.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/api/sockets.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/api/tcpip.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv4/autoip.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv4/dhcp.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv4/etharp.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv4/icmp.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv4/igmp.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv4/ip4.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv4/ip4_addr.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv4/ip4_frag.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv6/dhcp6.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv6/ethip6.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv6/icmp6.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv6/inet6.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv6/ip6.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv6/ip6_addr.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv6/ip6_frag.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv6/mld6.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ipv6/nd6.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/def.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/dns.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/inet_chksum.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/init.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/ip.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/mem.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/memp.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/netif.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/pbuf.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/raw.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/stats.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/sys.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/tcp.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/tcp_in.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/tcp_out.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/timeouts.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/core/udp.c
-    vendor/lwip/lwip-STABLE-2_0_2_RELEASE_VER/src/netif/ethernet.c
-
     vendor/modules/common/src/co_dlist.c
     vendor/modules/common/src/co_list.c
     vendor/modules/common/src/co_math.c
@@ -109,7 +115,4 @@ target_sources(${VSF_LIB_NAME} INTERFACE
     vendor/plf/aic8800/src/driver/trans/arm_cfft_radix4_q15.c
     vendor/plf/aic8800/src/driver/trng/trng_api.c
     vendor/plf/aic8800/src/driver/wdt/wdt_api.c
-
-    vendor/wifi/fhost/fhost_config.c
-    vendor/wifi/macif/rom_patch.c
 )
