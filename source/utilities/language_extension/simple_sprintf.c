f1011f83229c66b9df3b2a23d95e2499f3fbcbd5
vsfos <vsfos@foxmail.com>
2022-11-09 04:10:58 +0800

    [utilities/simple_printf] add VSF_SIMPLE_SPRINTF_SUPPORT_IPMAC

--- source/utilities/language_extension/simple_sprintf.c
+++ source/utilities/language_extension/simple_sprintf.c
@@ -168,6 +168,91 @@ int vsnprintf(char *str, size_t size, const char *format, va_list ap)
                 case 'P':
                     flags.is_upper = 1;
                 case 'p':
+                    arg.uinteger = (unsigned long long)va_arg(ap, void *);
+#if VSF_SIMPLE_SPRINTF_SUPPORT_IPMAC == ENABLED
+                    // use width as size, containing 1-byte NULL terminator
+                    width = size - realsize + 1;
+                    if (format[0] == 'M') {
+                        // %pM : printf mac address, XX:XX:XX:XX:XX:XX
+                        format++;
+                        if (format[0] == 'R') {
+                            format++;
+                            radix = snprintf(curpos, width, "%02X:%02X:%02X:%02X:%02X:%02X",
+                                arg.str[5], arg.str[4], arg.str[3], arg.str[2], arg.str[1], arg.str[0]);
+                        } else if (format[0] == 'F') {
+                            format++;
+                            radix = snprintf(curpos, width, "%02X-%02X-%02X-%02X-%02X-%02X",
+                                arg.str[0], arg.str[1], arg.str[2], arg.str[3], arg.str[4], arg.str[5]);
+                        } else {
+                            radix = snprintf(curpos, width, "%02X:%02X:%02X:%02X:%02X:%02X",
+                                arg.str[0], arg.str[1], arg.str[2], arg.str[3], arg.str[4], arg.str[5]);
+                        }
+                        realsize += radix;
+                        // even if curpos overflows, realsize will protect it
+                        curpos += radix;
+                        break;
+                    } else if (format[0] == 'm') {
+                        format++;
+                        if (format[0] == 'R') {
+                            format++;
+                            radix = snprintf(curpos, width, "%02X%02X%02X%02X%02X%02X",
+                                arg.str[5], arg.str[4], arg.str[3], arg.str[2], arg.str[1], arg.str[0]);
+                        } else {
+                            radix = snprintf(curpos, width, "%02X%02X%02X%02X%02X%02X",
+                                arg.str[0], arg.str[1], arg.str[2], arg.str[3], arg.str[4], arg.str[5]);
+                        }
+                        realsize += radix;
+                        // even if curpos overflows, realsize will protect it
+                        curpos += radix;
+                        break;
+                    } else if (format[0] == 'I') {
+                        if (format[1] == '4') {
+                            // %pI4 : printf IPv4, x.x.x.x
+                            format += 2;
+                            radix = snprintf(curpos, width, "%d.%d.%d.%d",
+                                arg.str[0], arg.str[1], arg.str[2], arg.str[3]);
+                            realsize += radix;
+                            // even if curpos overflows, realsize will protect it
+                            curpos += radix;
+                            break;
+                        } else if (format[1] == '6') {
+                            // %pI6 : printf IPv6, xxxx:xxxx:xxxx:xxxx:xxxx:xxxx:xxxx:xxxx
+                            format += 2;
+                            uint16_t *pu16 = (uint16_t *)arg.str;
+                            radix = snprintf(curpos, width, "%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X",
+                                pu16[0], pu16[1], pu16[2], pu16[3], pu16[4], pu16[5], pu16[6], pu16[7]);
+                            realsize += radix;
+                            // even if curpos overflows, realsize will protect it
+                            curpos += radix;
+                            break;
+                        }
+                    } else if (format[0] == 'i') {
+                        if (format[1] == '4') {
+                            // %pI4 : printf IPv4, x.x.x.x
+                            format += 2;
+                            radix = snprintf(curpos, width, "%03d.%03d.%03d.%03d",
+                                arg.str[0], arg.str[1], arg.str[2], arg.str[3]);
+                            realsize += radix;
+                            // even if curpos overflows, realsize will protect it
+                            curpos += radix;
+                            break;
+                        } else if (format[1] == '6') {
+                            // %pI6 : printf IPv6, xxxx:xxxx:xxxx:xxxx:xxxx:xxxx:xxxx:xxxx
+                            format += 2;
+                            uint16_t *pu16 = (uint16_t *)arg.str;
+                            radix = snprintf(curpos, width, "%02X%02X%02X%02X%02X%02X%02X%02X",
+                                pu16[0], pu16[1], pu16[2], pu16[3], pu16[4], pu16[5], pu16[6], pu16[7]);
+                            realsize += radix;
+                            // even if curpos overflows, realsize will protect it
+                            curpos += radix;
+                            break;
+                        }
+                    }
+#endif
+                    flags.is_signed = 0;
+                    radix = 16;
+                    width = 0;
+                    goto print_integer_do;
                 case 'x':
                     flags.is_signed = 0;
                     radix = 16;
