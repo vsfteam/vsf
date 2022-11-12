d778afbbaaa30eb6796dc5117c68cda0f9e1b9ea
vsfos <vsfos@foxmail.com>
2022-11-13 07:15:13 +0800

    [hal/aic8800] weaken vsf_driver_init

--- source/hal/driver/AIC/AIC8800/driver.c
+++ source/hal/driver/AIC/AIC8800/driver.c
@@ -97,6 +97,7 @@ void vsf_arch_shutdown(void)
  *  \retval true initialization succeeded.
  *  \retval false initialization failed
  */
+WEAK(vsf_driver_init)
 bool vsf_driver_init(void)
 {
     SystemInit();
