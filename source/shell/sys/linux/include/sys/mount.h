c0ba5e79fbcb1b7b1917339c23220e43cb031d29
vsfos <vsfos@foxmail.com>
2022-11-23 02:36:56 +0800

    [shell/linux/mount.h] add error message if VSF_USE_MAL is not enabled

--- source/shell/sys/linux/include/sys/mount.h
+++ source/shell/sys/linux/include/sys/mount.h
@@ -12,6 +12,10 @@ extern "C" {
 #define umount              VSF_LINUX_WRAPPER(umount)
 #endif
 
+#if VSF_USE_MAL != ENABLED
+#   error VSF_USE_MAL MUST be enabled to support mount
+#endif
+
 // fsdata if filesystem is not specified
 typedef struct vsf_linux_fsdata_auto_t {
     vk_mal_t *mal;
