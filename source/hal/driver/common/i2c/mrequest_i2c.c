58fd843c59996268921f117e34494450dd33a51f
Yongxiang Liang <tanekliang@gmail.com>
2022-12-12 17:16:37 +0800

    [hal/driver/i2c]: i2c master add get_transfered_count

--- source/hal/driver/common/i2c/mrequest_i2c.c
+++ source/hal/driver/common/i2c/mrequest_i2c.c
@@ -212,6 +212,17 @@ vsf_err_t vsf_mrequest_i2c_master_request(vsf_mrequest_i2c_t *m_i2c_ptr,
     return VSF_ERR_NOT_SUPPORT;
 }
 
+uint_fast32_t vsf_mrequest_i2c_get_transfered_count(vsf_mrequest_i2c_t *i2c_ptr)
+{
+    VSF_HAL_ASSERT(i2c_ptr != NULL);
+    VSF_HAL_ASSERT(i2c_ptr->op != NULL);
+    VSF_HAL_ASSERT(i2c_ptr->op->get_transfered_count!= NULL);
+
+	// TODO
+    VSF_HAL_ASSERT(0);
+    return 0;
+}
+
 vsf_i2c_capability_t vsf_mrequest_i2c_capability(vsf_mrequest_i2c_t *m_i2c_ptr)
 {
     VSF_HAL_ASSERT(NULL != m_i2c_ptr);
