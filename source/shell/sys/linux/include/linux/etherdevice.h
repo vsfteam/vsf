1303487392d5abd082369bf9ad7e13c1dd66c50b
vsfos <vsfos@foxmail.com>
2023-02-08 20:29:09 +0800

    [shell/linux/etherdevice] add is_broadcast_ether_addr

--- source/shell/sys/linux/include/linux/etherdevice.h
+++ source/shell/sys/linux/include/linux/etherdevice.h
@@ -10,6 +10,18 @@ static inline void eth_broadcast_addr(u8 *addr)
     memset(addr, 0xFF, ETH_ALEN);
 }
 
+static inline bool is_local_ether_addr(const u8 *addr)
+{
+    return 0x02 & addr[0];
+}
+
+static inline bool is_broadcast_ether_addr(const u8 *addr)
+{
+    return (*(const u16 *)(addr + 0) &
+            *(const u16 *)(addr + 2) &
+            *(const u16 *)(addr + 4)) == 0xffff;
+}
+
 #ifdef __cplusplus
 }
 #endif
