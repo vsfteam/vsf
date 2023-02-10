29177d6070d5f3c0cceefe6e79add967dcf925aa
vsfos <vsfos@foxmail.com>
2023-02-10 15:07:47 +0800

    [utilities/cpp] add cpp support

---
+++ source/utilities/language_extension/cpp/vsf_cpp.cpp
@@ -0,0 +1,47 @@
+/*****************************************************************************
+ *   Copyright(C)2009-2022 by VSF Team                                       *
+ *                                                                           *
+ *  Licensed under the Apache License, Version 2.0 (the "License");          *
+ *  you may not use this file except in compliance with the License.         *
+ *  You may obtain a copy of the License at                                  *
+ *                                                                           *
+ *     http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                           *
+ *  Unless required by applicable law or agreed to in writing, software      *
+ *  distributed under the License is distributed on an "AS IS" BASIS,        *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and      *
+ *  limitations under the License.                                           *
+ *                                                                           *
+ ****************************************************************************/
+
+
+/*============================ INCLUDES ======================================*/
+
+// for vsf_heap
+#include "service/vsf_service.h"
+
+/*============================ MACROS ========================================*/
+
+#if VSF_USE_HEAP != ENABLED
+#   error cpp support need VSF_HEAP
+#endif
+
+/*============================ MACROFIED FUNCTIONS ===========================*/
+/*============================ TYPES =========================================*/
+/*============================ PROTOTYPES ====================================*/
+/*============================ LOCAL VARIABLES ===============================*/
+/*============================ GLOBAL VARIABLES ==============================*/
+/*============================ IMPLEMENTATION ================================*/
+
+void *operator new(size_t size)
+{
+    return vsf_heap_malloc(size);
+}
+
+void operator delete(void *ptr)
+{
+    vsf_heap_free(ptr);
+}
+
+/* EOF */
