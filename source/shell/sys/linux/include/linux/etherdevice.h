ca797fbb3ef8a4165f2c23f8038cc85184c59ed4
vsfos <vsfos@foxmail.com>
2023-02-21 02:51:32 +0800

    [shell/linux] add is_valid_ether_addr

--- source/shell/sys/linux/include/linux/etherdevice.h
+++ source/shell/sys/linux/include/linux/etherdevice.h
@@ -22,6 +22,33 @@ static inline bool is_broadcast_ether_addr(const u8 *addr)
             *(const u16 *)(addr + 4)) == 0xffff;
 }
 
+static inline bool is_zero_ether_addr(const u8 *addr)
+{
+    return (*(const u16 *)(addr + 0) |
+            *(const u16 *)(addr + 2) |
+            *(const u16 *)(addr + 4)) == 0;
+}
+
+static inline bool is_multicast_ether_addr(const u8 *addr)
+{
+    u16 a = *(const u16 *)addr;
+#if __BYTE_ORDER == __BIG_ENDIAN
+    return 0x01 & (a >> ((sizeof(a) * 8) - 8));
+#else
+    return 0x01 & a;
+#endif
+}
+
+static inline bool is_unicast_ether_addr(const u8 *addr)
+{
+    return !is_multicast_ether_addr(addr);
+}
+
+static inline bool is_valid_ether_addr(const u8 *addr)
+{
+    return !is_multicast_ether_addr(addr) && !is_zero_ether_addr(addr);
+}
+
 #ifdef __cplusplus
 }
 #endif
