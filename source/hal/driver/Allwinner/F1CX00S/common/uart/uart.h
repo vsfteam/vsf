71d710d9e4ba8e66fdf421068baaae765624db02
Yongxiang Liang <tanekliang@gmail.com>
2023-02-22 15:54:21 +0800

    [hal/driver/f1cx00s] update usart

--- source/hal/driver/Allwinner/F1CX00S/common/uart/uart.h
+++ source/hal/driver/Allwinner/F1CX00S/common/uart/uart.h
@@ -21,90 +21,139 @@
 /*============================ INCLUDES ======================================*/
 
 #include "hal/vsf_hal_cfg.h"
-#include "../../__device.h"
 
 #if VSF_HAL_USE_USART == ENABLED
 
-#ifndef VSF_HAL_USART_IMP_REQUEST_BY_FIFO
-#   define VSF_HAL_USART_IMP_REQUEST_BY_FIFO    ENABLED
-#endif
+#include "../../__device.h"
+#include "hal/driver/common/template/vsf_template_hal_driver.h"
 
 /*============================ MACROS ========================================*/
+
+#define VSF_USART_CFG_REIMPLEMENT_TYPE_MODE      ENABLED
+#define VSF_USART_CFG_REIMPLEMENT_TYPE_IRQ_MASK  ENABLED
+#define VSF_USART_CFG_REIMPLEMENT_TYPE_STATUS    ENABLED
+
 /*============================ MACROFIED FUNCTIONS ===========================*/
 /*============================ TYPES =========================================*/
 
-enum em_clk_reg_rw_t {
-    USART1_CLK_EN           = 0x04000U,
-    USART2_CLK_EN           = 0x20000U,
-    USART3_CLK_EN           = 0x40000U,
 
-    GPIOA_CLK_EN            = 0X00004U,
-    GPIOB_CLK_EN            = 0X00008U,
-};
+typedef enum vsf_usart_mode_t {
+    VSF_USART_NO_PARITY                   = 0x0000U,
+    VSF_USART_EVEN_PARITY                 = LCR_PEN | LCR_EPS_EVEN,
+    VSF_USART_ODD_PARITY                  = LCR_PEN | LCR_EPS_ODD,
+    F1C100S_USART_PARITY_MASK             = VSF_USART_NO_PARITY |
+                                            VSF_USART_EVEN_PARITY |
+                                            VSF_USART_ODD_PARITY,
 
-enum em_gpio_reg_rw_t {
-    USART1_GPIO_MODE_CLEAR  = 0XFFFFF00FU,
-    USART1_GPIO_MODE        = 0x000008B0U,
+    VSF_USART_1_STOPBIT                   = LCR_STOP_1,
+    VSF_USART_2_STOPBIT                   = LCR_STOP_2,
+    F1C100S_USART_STOPBIT_MASK            = VSF_USART_1_STOPBIT |
+                                            VSF_USART_2_STOPBIT,
 
-    USART2_GPIO_MODE_CLEAR  = 0XFFFF00FFU,
-    USART2_GPIO_MODE        = 0x00008B00U,
+    VSF_USART_5_BIT_LENGTH                = LCR_DLS_5,
+    VSF_USART_6_BIT_LENGTH                = LCR_DLS_6,
+    VSF_USART_7_BIT_LENGTH                = LCR_DLS_7,
+    VSF_USART_8_BIT_LENGTH                = LCR_DLS_8,
+    F1C100S_USART_BIT_LENGTH_MASK         = VSF_USART_5_BIT_LENGTH |
+                                            VSF_USART_6_BIT_LENGTH |
+                                            VSF_USART_7_BIT_LENGTH |
+                                            VSF_USART_8_BIT_LENGTH,
 
-    USART3_GPIO_MODE_CLEAR  = 0XFFFF00FFU,
-    USART3_GPIO_MODE        = 0x00008B00U,
-};
+    VSF_USART_RTS_CTS_HWCONTROL           = MCR_AFCE << 8,
+    F1C100S_USART_HWCONTROL_MASK          = VSF_USART_RTS_CTS_HWCONTROL,
 
-enum vsf_usart_mode_t {
-    VSF_USART_5_BIT_LENGTH      = LCR_DLS_5,
-    VSF_USART_6_BIT_LENGTH      = LCR_DLS_6,
-    VSF_USART_7_BIT_LENGTH      = LCR_DLS_7,
-    VSF_USART_8_BIT_LENGTH      = LCR_DLS_8,
+    F1C100S_USART_IRDA_SIR_MODE           = MCR_SIRE << 8,
+    F1C100S_USART_LOOP_BACK_MODE          = MCR_LOOP << 8,
 
-    VSF_USART_1_STOPBIT         = LCR_STOP_1,
-    VSF_USART_2_STOPBIT         = LCR_STOP_2,
+    F1C100S_USART_TX_FIFO_THRES_EMPTY     = FCR_TFT_EMPTY   << 16,
+    F1C100S_USART_TX_FIFO_THRES_2         = FCR_TFT_2       << 16,
+    F1C100S_USART_TX_FIFO_THRES_QUARTER   = FCR_TFT_QUARTER << 16,
+    F1C100S_USART_TX_FIFO_THRES_HALF      = FCR_TFT_HALF    << 16,
+    F1C100S_USART_TX_FIFO_THRES_MASK      = F1C100S_USART_TX_FIFO_THRES_EMPTY |
+                                              F1C100S_USART_TX_FIFO_THRES_2 |
+                                              F1C100S_USART_TX_FIFO_THRES_QUARTER |
+                                              F1C100S_USART_TX_FIFO_THRES_HALF,
 
-    VSF_USART_NO_PARITY         = 0x0000U,
-    VSF_USART_EVEN_PARITY       = LCR_PEN | LCR_EPS_EVEN,
-    VSF_USART_ODD_PARITY        = LCR_PEN | LCR_EPS_ODD,
+    F1C100S_USART_RX_FIFO_THRES_1         = FCR_RT_1        << 16,
+    F1C100S_USART_RX_FIFO_THRES_QUARTER   = FCR_RT_QUARTER  << 16,
+    F1C100S_USART_RX_FIFO_THRES_HALF      = FCR_RT_HALF     << 16,
+    F1C100S_USART_RX_FIFO_THRES_2_LESS    = FCR_RT_2_LESS   << 16,
+    F1C100S_USART_RX_FIFO_THRES_MASK      = F1C100S_USART_RX_FIFO_THRES_1 |
+                                            F1C100S_USART_RX_FIFO_THRES_QUARTER |
+                                            F1C100S_USART_RX_FIFO_THRES_HALF |
+                                            F1C100S_USART_RX_FIFO_THRES_HALF,
 
-    USART_LOOP_BACK_MODE        = MCR_LOOP << 8,
-    USART_AUTO_FLOW_CONTROL     = MCR_AFCE << 8,
-    USART_IRDA_SIR_MODE         = MCR_SIRE << 8,
+    F1C100S_USART_MODE_ALL_BITS_MASK      = F1C100S_USART_PARITY_MASK |
+                                            F1C100S_USART_STOPBIT_MASK |
+                                            F1C100S_USART_BIT_LENGTH_MASK |
+                                            F1C100S_USART_HWCONTROL_MASK |
+                                            F1C100S_USART_IRDA_SIR_MODE |
+                                            F1C100S_USART_LOOP_BACK_MODE |
+                                            F1C100S_USART_TX_FIFO_THRES_MASK |
+                                            F1C100S_USART_RX_FIFO_THRES_MASK,
 
-    USART_TX_FIFO_THRES_EMPTY   = FCR_TFT_EMPTY   << 16,
-    USART_TX_FIFO_THRES_2       = FCR_TFT_2       << 16,
-    USART_TX_FIFO_THRES_QUARTER = FCR_TFT_QUARTER << 16,
-    USART_TX_FIFO_THRES_HALF    = FCR_TFT_HALF    << 16,
+    VSF_USART_FORCE_0_PARITY              = (0x1ul << 31),
+    VSF_USART_FORCE_1_PARITY              = (0x1ul << 30),
+    VSF_USART_PARITY_MASK                 = F1C100S_USART_PARITY_MASK |
+                                            VSF_USART_FORCE_0_PARITY |
+                                            VSF_USART_FORCE_1_PARITY,
 
-    USART_RX_FIFO_THRES_1       = FCR_RT_1        << 16,
-    USART_RX_FIFO_THRES_QUARTER = FCR_RT_QUARTER  << 16,
-    USART_RX_FIFO_THRES_HALF    = FCR_RT_HALF     << 16,
-    USART_RX_FIFO_THRES_2_LESS  = FCR_RT_2_LESS   << 16,
+    VSF_USART_1_5_STOPBIT                 = (0x1ul << 29),
+    VSF_USART_STOPBIT_MASK                = F1C100S_USART_STOPBIT_MASK |
+                                            VSF_USART_1_5_STOPBIT,
 
-    // TODO:
-    VSF_USART_TX_ENABLE             = 0,
-    VSF_USART_RX_ENABLE             = 0,
-};
+    VSF_USART_9_BIT_LENGTH                = (0x4ul << 28),
+    VSF_USART_BIT_LENGTH_MASK             = F1C100S_USART_BIT_LENGTH_MASK |
+                                            VSF_USART_9_BIT_LENGTH,
 
-enum vsf_usart_irq_mask_t {
+    VSF_USART_NO_HWCONTROL                = (0x0ul << 26),
+    VSF_USART_RTS_HWCONTROL               = (0x1ul << 26),
+    VSF_USART_CTS_HWCONTROL               = (0x2ul << 26),
+    VSF_USART_HWCONTROL_MASK              = F1C100S_USART_HWCONTROL_MASK |
+                                            VSF_USART_NO_HWCONTROL |
+                                            VSF_USART_RTS_HWCONTROL |
+                                            VSF_USART_CTS_HWCONTROL,
+
+    VSF_USART_TX_ENABLE                   = (0x0ul << 25),
+    VSF_USART_RX_ENABLE                   = (0x0ul << 24),
+    VSF_USART_ENABLE_MASK                 = VSF_USART_TX_ENABLE |
+                                            VSF_USART_RX_ENABLE,
+
+    VSF_USART_MODE_ALL_BITS_MASK          = VSF_USART_PARITY_MASK |
+                                            VSF_USART_STOPBIT_MASK |
+                                            VSF_USART_BIT_LENGTH_MASK |
+                                            VSF_USART_HWCONTROL_MASK |
+                                            VSF_USART_ENABLE_MASK,
+} vsf_usart_mode_t;
+
+typedef enum vsf_usart_irq_mask_t {
     VSF_USART_IRQ_MASK_RX           = IER_ERBFI,
     VSF_USART_IRQ_MASK_TX           = IER_ETBEI,
 
-#ifdef VSF_HAL_USART_IMP_REQUEST_BY_FIFO
-    VSF_USART_IRQ_MASK_TX_CPL       = (1UL << 2),
-    VSF_USART_IRQ_MASK_RX_CPL       = (1UL << 3),
-#endif
-};
+    F1C100S_USART_IRQ_ALL_BITS_MASK = VSF_USART_IRQ_MASK_RX |
+                                      VSF_USART_IRQ_MASK_TX,
 
-/*============================ INCLUDES ======================================*/
+    VSF_USART_IRQ_MASK_TX_CPL       = (0x1ul << 23),
+    VSF_USART_IRQ_MASK_RX_CPL       = (0x1ul << 24),
+    VSF_USART_IRQ_MASK_RX_TIMEOUT       = (0x1ul << 27),
 
-#include "hal/driver/common/template/vsf_template_usart.h"
-#if VSF_HAL_USART_IMP_REQUEST_BY_FIFO == ENABLED
-#   include "hal/driver/common/usart/__usart_common.h"
-#endif
+    VSF_USART_IRQ_MASK_FRAME_ERR    = (0x1ul << 28),
+    VSF_USART_IRQ_MASK_PARITY_ERR   = (0x1ul << 29),
+    VSF_USART_IRQ_MASK_BREAK_ERR    = (0x1ul << 30),
+    VSF_USART_IRQ_MASK_OVERFLOW_ERR = (0x1ul << 31),
+    VSF_USART_IRQ_MASK_ERR          = VSF_USART_IRQ_MASK_FRAME_ERR |
+                                      VSF_USART_IRQ_MASK_PARITY_ERR |
+                                      VSF_USART_IRQ_MASK_BREAK_ERR |
+                                      VSF_USART_IRQ_MASK_OVERFLOW_ERR,
 
-/*============================ TYPES =========================================*/
+    VSF_USART_IRQ_ALL_BITS_MASK     = F1C100S_USART_IRQ_ALL_BITS_MASK |
+                                      VSF_USART_IRQ_MASK_RX_TIMEOUT |
+                                      VSF_USART_IRQ_MASK_TX_CPL |
+                                      VSF_USART_IRQ_MASK_RX_CPL |
+                                      VSF_USART_IRQ_MASK_ERR,
+} vsf_usart_irq_mask_t;
 
-struct vsf_usart_status_t {
+typedef struct vsf_usart_status_t {
     union {
         inherit(vsf_peripheral_status_t)
         struct {
@@ -112,16 +161,16 @@ struct vsf_usart_status_t {
             uint32_t        ir      : 8;
         };
     };
-};
+} vsf_usart_status_t;
 
-struct vsf_usart_t {
-    uart_reg_t            * reg;
+/*============================ INCLUDES ======================================*/
 
-    IRQn_Type               UART_IRQn;
-    vsf_usart_isr_t         isr;
+#define VSF_USART_CFG_DEC_PREFIX            vsf_hw
+#define VSF_USART_CFG_DEC_UPCASE_PREFIX     VSF_HW
+#include "hal/driver/common/usart/usart_template.h"
 
-    vsf_hal_usart_def_req_by_fifo()
-};
+/*============================ TYPES =========================================*/
+
 
 /*============================ GLOBAL VARIABLES ==============================*/
 /*============================ PROTOTYPES ====================================*/
