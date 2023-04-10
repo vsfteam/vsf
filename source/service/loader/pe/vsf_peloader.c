c20767974027832601a83f131114029eb0da1b73
vsfos <vsfos@foxmail.com>
2023-04-10 13:25:39 +0800

    [service/peloader] support export

--- source/service/loader/pe/vsf_peloader.c
+++ source/service/loader/pe/vsf_peloader.c
@@ -42,6 +42,12 @@
 #endif
 
 /*============================ TYPES =========================================*/
+
+typedef struct {
+   WORD                     offset : 12;
+   WORD                     type : 4;
+} IMAGE_RELOC, *PIMAGE_RELOC;
+
 /*============================ PROTOTYPES ====================================*/
 /*============================ LOCAL VARIABLES ===============================*/
 /*============================ GLOBAL VARIABLES ==============================*/
@@ -89,30 +95,34 @@ int vsf_peloader_load(vsf_peloader_t *peloader, vsf_loader_target_t *target)
         goto free_and_fail;
     }
     IMAGE_NT_HEADERS *ntheader_ram = (IMAGE_NT_HEADERS *)(ram_base + dosheader.e_lfanew);
+    ULONG_PTR vaddr_offset = (ULONG_PTR)(ram_base  - ntheader_ram->OptionalHeader.ImageBase);
+    vsf_peloader_debug("remap vaddr 0x%p to paddr 0x%p" VSF_TRACE_CFG_LINEEND, ntheader_ram->OptionalHeader.ImageBase, ram_base);
     ntheader_ram->OptionalHeader.ImageBase = (uintptr_t)ram_base;
 
     IMAGE_SECTION_HEADER *secheader = (IMAGE_SECTION_HEADER *)((char *)&ntheader_ram->OptionalHeader + ntheader_ram->FileHeader.SizeOfOptionalHeader);
     for (WORD i = 0; i < ntheader_ram->FileHeader.NumberOfSections; i++, secheader++) {
         if ((secheader->VirtualAddress + secheader->SizeOfRawData) > ntheader_ram->OptionalHeader.SizeOfImage) {
-            vsf_trace_error("section %d overflows" VSF_TRACE_CFG_LINEEND, i);
+            vsf_trace_error("section %s overflows" VSF_TRACE_CFG_LINEEND, secheader->Name);
             goto free_and_fail;
         }
+        vsf_peloader_debug("load section %s at 0x%08X" VSF_TRACE_CFG_LINEEND, secheader->Name, secheader->VirtualAddress);
         if (vsf_loader_read(target, secheader->PointerToRawData, ram_base + secheader->VirtualAddress, secheader->SizeOfRawData) != secheader->SizeOfRawData) {
-            vsf_trace_error("fail to load section %d" VSF_TRACE_CFG_LINEEND, i);
+            vsf_trace_error("fail to load section %s" VSF_TRACE_CFG_LINEEND, secheader->Name);
             goto free_and_fail;
         }
     }
 
     IMAGE_DATA_DIRECTORY *data_dir = &ntheader_ram->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT];
     DWORD vaddr = data_dir->VirtualAddress;
-    if (vaddr != 0) {
+    DWORD size = data_dir->Size;
+    if ((vaddr != 0) && (size > 0)) {
         IMAGE_IMPORT_DESCRIPTOR *import_desc = (IMAGE_IMPORT_DESCRIPTOR *)(ram_base + vaddr);
         IMAGE_THUNK_DATA *thunk_data_out;
         IMAGE_THUNK_DATA *thunk_data_in;
         IMAGE_IMPORT_BY_NAME *import_name;
 
         while (import_desc->Name != 0) {
-            vsf_peloader_debug("loading %s" VSF_TRACE_CFG_LINEEND, (char *)(ram_base + import_desc->Name));
+            vsf_peloader_debug("import library %s" VSF_TRACE_CFG_LINEEND, (char *)(ram_base + import_desc->Name));
 
             thunk_data_out = (IMAGE_THUNK_DATA *)(ram_base + import_desc->FirstThunk);
             thunk_data_in = import_desc->OriginalFirstThunk ?
@@ -125,13 +135,91 @@ int vsf_peloader_load(vsf_peloader_t *peloader, vsf_loader_target_t *target)
                     goto free_and_fail;
                 } else {
                     import_name = (IMAGE_IMPORT_BY_NAME *)(ram_base + thunk_data_in->u1.AddressOfData);
-                    vsf_peloader_debug("  reloate %s" VSF_TRACE_CFG_LINEEND, import_name->Name);
+                    vsf_peloader_debug("  import function %s" VSF_TRACE_CFG_LINEEND, import_name->Name);
                     thunk_data_out->u1.Function = (uintptr_t)0;
                 }
                 thunk_data_out++;
                 thunk_data_in++;
             }
             import_desc++;
+        }
+    }
+
+    data_dir = &ntheader_ram->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC];
+    vaddr = data_dir->VirtualAddress;
+    size = data_dir->Size;
+    if ((vaddr != 0) && (size > 0)) {
+        IMAGE_BASE_RELOCATION *reloc = (IMAGE_BASE_RELOCATION *)(ram_base + vaddr);
+        IMAGE_RELOC *rel_info = (IMAGE_RELOC *)&reloc[1];
+        char *ptr = ram_base + reloc->VirtualAddress;
+
+        while (reloc->VirtualAddress != 0) {
+            for (DWORD i = 0; i < ((reloc->SizeOfBlock - sizeof(IMAGE_BASE_RELOCATION)) / sizeof(IMAGE_RELOC)); i++, rel_info++) {
+                switch (rel_info->type) {
+                case IMAGE_REL_BASED_DIR64:     *((ULONG_PTR*)(ptr + rel_info->offset)) += vaddr_offset;    break;
+                case IMAGE_REL_BASED_HIGHLOW:   *((DWORD*)(ptr + rel_info->offset)) += (DWORD)vaddr_offset; break;
+                case IMAGE_REL_BASED_HIGH:      *((WORD*)(ptr + rel_info->offset)) += HIWORD(vaddr_offset); break;
+                case IMAGE_REL_BASED_LOW:       *((WORD*)(ptr + rel_info->offset)) += LOWORD(vaddr_offset); break;
+                case IMAGE_REL_BASED_ABSOLUTE:                                                              break;
+                default:                                                                                    break;
+                }
+            }
+            reloc = (IMAGE_BASE_RELOCATION *)((char *)reloc + reloc->SizeOfBlock);
+        }
+    }
+
+    if (peloader->alloc_vplt != NULL) {
+        data_dir = &ntheader_ram->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT];
+        vaddr = data_dir->VirtualAddress;
+        size = data_dir->Size;
+
+        if ((vaddr != 0) && (size > 0)) {
+            IMAGE_EXPORT_DIRECTORY *export = (IMAGE_EXPORT_DIRECTORY *)(ram_base + vaddr);
+            DWORD func_vaddr = export->AddressOfFunctions;
+            DWORD name_vaddr = export->AddressOfNames;
+            DWORD ord_vaddr = export->AddressOfNameOrdinals;
+            vsf_peloader_debug("export library %s" VSF_TRACE_CFG_LINEEND, (char *)(ram_base + export->Name));
+
+            char *name;
+            WORD nameidx;
+            void *func;
+            int namelen = 0;
+            for (DWORD i = 0; i < export->NumberOfNames; i++) {
+                name = ram_base + *(DWORD *)(ram_base + name_vaddr + i * sizeof(DWORD));
+                nameidx = *(WORD *)(ram_base + ord_vaddr + i * sizeof(WORD));
+                func = ram_base + *(DWORD *)(ram_base + func_vaddr + nameidx * sizeof(DWORD));
+
+                if (func != 0) {
+                    namelen += strlen(name) + 1;
+                }
+            }
+
+            int total_byte_size = sizeof(vsf_vplt_info_t) + export->NumberOfNames * sizeof(vsf_vplt_entry_t) + namelen;
+            peloader->vplt_out = peloader->alloc_vplt(total_byte_size);
+            if (NULL == peloader->vplt_out) {
+                goto free_and_fail;
+            }
+
+            vsf_vplt_info_t *vplt_info = peloader->vplt_out;
+            vplt_info->entry_num = export->NumberOfNames;
+            vplt_info->final = true;
+            vplt_info->major = 0;
+            vplt_info->minor = 0;
+            vsf_vplt_entry_t *vplt_entry = (vsf_vplt_entry_t *)&vplt_info[1];
+            char *cur_name = (char *)vplt_entry + export->NumberOfNames * sizeof(vsf_vplt_entry_t);
+            DWORD func_ord;
+            for (DWORD i = 0; i < export->NumberOfNames; i++) {
+                name = ram_base + *(DWORD *)(ram_base + name_vaddr + i * sizeof(DWORD));
+                nameidx = *(WORD *)(ram_base + ord_vaddr + i * sizeof(WORD));
+                func = ram_base + *(DWORD *)(ram_base + func_vaddr + nameidx * sizeof(DWORD));
+
+                strcpy(cur_name, name);
+                vplt_entry->name = cur_name;
+                cur_name += strlen(name);
+                vplt_entry->ptr = func;
+                vsf_peloader_debug("  export function %s at 0x%p" VSF_TRACE_CFG_LINEEND, name, vplt_entry->ptr);
+                vplt_entry++;
+            }
         }
     }
 
