824d811f66a2bbe3b9e31aab3f4b1d762ede90ff
vsfos <vsfos@foxmail.com>
2023-04-16 14:52:20 +0800

    [utilities/compiler] minor tweak to avoid compile warnings if some MACROs has already been defined

--- source/utilities/compiler/__common/__compiler.h
+++ source/utilities/compiler/__common/__compiler.h
@@ -93,9 +93,15 @@ extern "C" {
 #       define NO_INIT          __attribute__((__section__(".bss.noinit")))
 #   endif
 #   define ROOT                 __attribute__((__used__))
-#   define INLINE               __inline__
-#   define NO_INLINE            __attribute__((__noinline__))
-#   define ALWAYS_INLINE        __inline__ __attribute__((__always_inline__))
+#   ifndef INLINE
+#       define INLINE           __inline__
+#   endif
+#   ifndef NO_INLINE
+#       define NO_INLINE        __attribute__((__noinline__))
+#   endif
+#   ifndef ALWAYS_INLINE
+#       define ALWAYS_INLINE    __inline__ __attribute__((__always_inline__))
+#   endif
 #   define WEAK(...)            __attribute__((__weak__))
 #   ifdef __APPLE__
 #       define RAMFUNC          Not Supported by Apple LLVM
@@ -114,7 +120,9 @@ extern "C" {
                                 __asm__(".weak " #__ALIAS);                     \
                                 __asm__(".equ " #__ALIAS ", " #__ORIGIN)
 
-#   define PACKED               __attribute__((__packed__))
+#   ifndef PACKED
+#       define PACKED           __attribute__((__packed__))
+#   endif
 //#   define UNALIGNED            __attribute__((__packed__))
 #   undef UNALIGNED                                                             //! llvm doesn't support this
 #   define TRANSPARENT_UNION    __attribute__((__transparent_union__))
@@ -126,9 +134,15 @@ extern "C" {
 #elif  __IS_COMPILER_GCC__
 #   define NO_INIT              __attribute__((section(".bss.noinit")))
 #   define ROOT                 __attribute__((used))
-#   define INLINE               inline
-#   define NO_INLINE            __attribute__((noinline))
-#   define ALWAYS_INLINE        inline __attribute__((always_inline))
+#   ifndef INLINE
+#       define INLINE           inline
+#   endif
+#   ifndef NO_INLINE
+#       define NO_INLINE        __attribute__((noinline))
+#   endif
+#   ifndef ALWAYS_INLINE
+#       define ALWAYS_INLINE    inline __attribute__((always_inline))
+#   endif
 #   define WEAK(...)            __attribute__((weak))
 #   define RAMFUNC              __attribute__((section(".textrw")))
 #   define __asm__              __asm
@@ -138,7 +152,9 @@ extern "C" {
 #   define __WEAK_ALIAS(__ORIGIN, __ALIAS) \
                                 __attribute__((weakref(__VSF_STR(__ALIAS))))
 
-#   define PACKED               __attribute__((packed))
+#   ifndef PACKED
+#       define PACKED           __attribute__((packed))
+#   endif
 //#   define UNALIGNED            __attribute__((packed))
 #   undef UNALIGNED                                                             //! gcc doesn't support this
 #   define TRANSPARENT_UNION    __attribute__((transparent_union))
