d544e948223346faff5c12692c0dc8269d69e4e4
vsfos <vsfos@foxmail.com>
2023-04-26 04:28:09 +0800

    [shell/linux/assert] update

--- source/shell/sys/linux/include/simple_libc/assert/assert.h
+++ source/shell/sys/linux/include/simple_libc/assert/assert.h
@@ -1,12 +1,11 @@
-#ifndef __SIMPLE_LIBC_ASSERT_H__
-#define __SIMPLE_LIBC_ASSERT_H__
-
 // for VSF_ASSERT
 #include "utilities/compiler/compiler.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#undef assert
 
 #ifdef NDEBUG
 #   define assert(...)          ((void)0)
@@ -16,6 +15,4 @@ extern "C" {
 
 #ifdef __cplusplus
 }
-#endif
-
 #endif
