539bc3b2b292fe088fd26f45b34f872b2d30bbd1
Yongxiang Liang <tanekliang@gmail.com>
2023-05-26 21:56:52 +0800

    [example/template/demo] add all hal demos to busybox_bind

--- example/template/demo/linux_demo/linux_demo.c
+++ example/template/demo/linux_demo/linux_demo.c
@@ -258,39 +258,51 @@ int vsf_linux_create_fhs(void)
 #if APP_USE_HAL_DEMO == ENABLED
 #   if APP_USE_HAL_ADC_DEMO == ENABLED && VSF_HAL_USE_ADC == ENABLED
     extern int adc_main(int argc, char *argv[]);
-    busybox_bind(VSF_LINUX_CFG_BIN_PATH "/adc", adc_main);
+    busybox_bind(VSF_LINUX_CFG_BIN_PATH "/adc-test", adc_main);
 #   endif
 #   if APP_USE_HAL_FLASH_DEMO == ENABLED && VSF_HAL_USE_FLASH == ENABLED
     extern int flash_main(int argc, char *argv[]);
-    busybox_bind(VSF_LINUX_CFG_BIN_PATH "/flash", flash_main);
+    busybox_bind(VSF_LINUX_CFG_BIN_PATH "/flash-test", flash_main);
 #   endif
 #   if APP_USE_HAL_GPIO_DEMO == ENABLED && VSF_HAL_USE_GPIO == ENABLED
     extern int gpio_main(int argc, char *argv[]);
-    busybox_bind(VSF_LINUX_CFG_BIN_PATH "/gpio", gpio_main);
+    busybox_bind(VSF_LINUX_CFG_BIN_PATH "/gpio-test", gpio_main);
 #   endif
 #   if APP_USE_HAL_I2C_DEMO == ENABLED && VSF_HAL_USE_GPIO == ENABLED
     extern int i2c_main(int argc, char *argv[]);
-    busybox_bind(VSF_LINUX_CFG_BIN_PATH "/i2c", i2c_main);
+    busybox_bind(VSF_LINUX_CFG_BIN_PATH "/i2c-test", i2c_main);
+#   endif
+#   if APP_USE_HAL_IO_DEMO == ENABLED && VSF_HAL_USE_GPIO == ENABLED
+    extern int io_main(int argc, char *argv[]);
+    busybox_bind(VSF_LINUX_CFG_BIN_PATH "/io-test", io_main);
+#   endif
+#   if APP_USE_HAL_PM_DEMO == ENABLED && VSF_HAL_USE_GPIO == ENABLED
+    extern int pm_main(int argc, char *argv[]);
+    busybox_bind(VSF_LINUX_CFG_BIN_PATH "/pm-test", pm_main);
 #   endif
 #   if APP_USE_HAL_PWM_DEMO == ENABLED && VSF_HAL_USE_PWM == ENABLED
     extern int pwm_main(int argc, char *argv[]);
-    busybox_bind(VSF_LINUX_CFG_BIN_PATH "/pwm", pwm_main);
+    busybox_bind(VSF_LINUX_CFG_BIN_PATH "/pwm-test", pwm_main);
+#   endif
+#   if APP_USE_HAL_RNG_DEMO == ENABLED && VSF_HAL_USE_GPIO == ENABLED
+    extern int rng_main(int argc, char *argv[]);
+    busybox_bind(VSF_LINUX_CFG_BIN_PATH "/rng-test", rng_main);
 #   endif
 #   if APP_USE_HAL_RTC_DEMO == ENABLED && VSF_HAL_USE_GPIO == ENABLED
     extern int rtc_main(int argc, char *argv[]);
-    busybox_bind(VSF_LINUX_CFG_BIN_PATH "/rtc", rtc_main);
+    busybox_bind(VSF_LINUX_CFG_BIN_PATH "/rtc-test", rtc_main);
 #   endif
 #   if APP_USE_HAL_SPI_DEMO == ENABLED && VSF_HAL_USE_GPIO == ENABLED
     extern int spi_main(int argc, char *argv[]);
-    busybox_bind(VSF_LINUX_CFG_BIN_PATH "/spi", spi_main);
+    busybox_bind(VSF_LINUX_CFG_BIN_PATH "/spi-test", spi_main);
 #   endif
 #   if APP_USE_HAL_TIMER_DEMO == ENABLED && VSF_HAL_USE_TIMER == ENABLED
     extern int timer_main(int argc, char *argv[]);
-    busybox_bind(VSF_LINUX_CFG_BIN_PATH "/timer", timer_main);
+    busybox_bind(VSF_LINUX_CFG_BIN_PATH "/timer-test", timer_main);
 #   endif
 #   if APP_USE_HAL_USART_DEMO == ENABLED && VSF_HAL_USE_USART == ENABLED
     extern int usart_main(int argc, char *argv[]);
-    busybox_bind(VSF_LINUX_CFG_BIN_PATH "/usart", usart_main);
+    busybox_bind(VSF_LINUX_CFG_BIN_PATH "/usart-test", usart_main);
 #   endif
 #   if APP_USE_HAL_WDT_DEMO == ENABLED && VSF_HAL_USE_WDT == ENABLED
     extern int wdt_main(int argc, char *argv[]);
