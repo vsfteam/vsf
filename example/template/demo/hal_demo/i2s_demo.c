3151df882e18efa1dcbb97607bf0b2e5c362641d
Yongxiang Liang <tanekliang@gmail.com>
2023-05-26 19:39:56 +0800

    [example/template/demo] update i2s demo

---
+++ example/template/demo/hal_demo/i2s_demo.c
@@ -0,0 +1,51 @@
+/*****************************************************************************
+ *   Copyright(C)2009-2022 by VSF Team                                       *
+ *                                                                           *
+ *  Licensed under the Apache License, Version 2.0 (the "License");          *
+ *  you may not use this file except in compliance with the License.         *
+ *  You may obtain a copy of the License at                                  *
+ *                                                                           *
+ *     http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                           *
+ *  Unless required by applicable law or agreed to in writing, software      *
+ *  distributed under the License is distributed on an "AS IS" BASIS,        *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and      *
+ *  limitations under the License.                                           *
+ *                                                                           *
+ ****************************************************************************/
+
+/*============================ INCLUDES ======================================*/
+
+#include "vsf.h"
+
+#if APP_USE_HAL_DEMO == ENABLED && APP_USE_HAL_I2S_DEMO == ENABLED && VSF_HAL_USE_I2S == ENABLED
+
+#if VSF_USE_LINUX == ENABLED
+#   include <getopt.h>
+#endif
+
+#include "hal_demo.h"
+
+/*============================ MACROS ========================================*/
+/*============================ MACROFIED FUNCTIONS ===========================*/
+/*============================ TYPES =========================================*/
+/*============================ GLOBAL VARIABLES ==============================*/
+/*============================ PROTOTYPES ====================================*/
+/*============================ LOCAL VARIABLES ===============================*/
+/*============================ PROTOTYPES ====================================*/
+/*============================ IMPLEMENTATION ================================*/
+
+#if APP_USE_LINUX_DEMO == ENABLED
+int i2s_main(int argc, char *argv[])
+{
+    return 0;
+}
+#else
+int VSF_USER_ENTRY(void)
+{
+    return 0;
+}
+#endif
+
+#endif
