17a0917b38bffd14318d77a4fc07e202a35071aa
vsfos <vsfos@foxmail.com>
2024-03-04 22:38:13 +0800

    [arch/riscv] update

--- source/hal/arch/rv/rv_generic.h
+++ source/hal/arch/rv/rv_generic.h
@@ -75,24 +75,22 @@ static ALWAYS_INLINE vsf_gint_state_t vsf_get_interrupt(void)
 static ALWAYS_INLINE vsf_gint_state_t vsf_set_interrupt(vsf_gint_state_t level)
 {
     vsf_gint_state_t result;
-    __asm volatile("csrrw %0, mstatus, %1" : "=r"(result) : "r"(level));
-    return result;
+    __asm volatile("csrrs %0, mstatus, %1" : "=r"(result) : "r"(level));
+    return result & 8;
 }
 
 static ALWAYS_INLINE vsf_gint_state_t vsf_disable_interrupt(void)
 {
     vsf_gint_state_t result;
-    uint32_t bits = 8;
-    __asm volatile("csrrc %0, mstatus, %1" : "=r"(result) : "r"(bits));
-    return result;
+    __asm volatile("csrrci %0, mstatus, 8" : "=r"(result) :);
+    return result & 8;
 }
 
 static ALWAYS_INLINE vsf_gint_state_t vsf_enable_interrupt(void)
 {
     vsf_gint_state_t result;
-    uint32_t bits = 8;
-    __asm volatile("csrrs %0, mstatus, %1" : "=r"(result) : "r"(bits));
-    return result;
+    __asm volatile("csrrsi %0, mstatus, 8" : "=r"(result) :);
+    return result & 8;
 }
 
 static ALWAYS_INLINE void vsf_arch_sleep(uint_fast32_t mode)
