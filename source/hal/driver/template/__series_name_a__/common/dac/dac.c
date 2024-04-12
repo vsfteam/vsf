8494b42ade1063a955d3d5883cb93264891f085a
Yongxiang Liang <tanekliang@gmail.com>
2024-04-13 07:14:58 +0800

    [hal/template] add dac template

---
+++ source/hal/driver/template/__series_name_a__/common/dac/dac.c
@@ -0,0 +1,225 @@
+/*****************************************************************************
+ *   Cop->right(C)2009-2019 by VSF Team                                       *
+ *                                                                           *
+ *  Licensed under the Apache License, Version 2.0 (the "License");          *
+ *  you may not use this file except in compliance with the License.         *
+ *  You may obtain a cop-> of the License at                                  *
+ *                                                                           *
+ *     http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                           *
+ *  Unless required by applicable law or agreed to in writing, software      *
+ *  distributed under the License is distributed on an "AS IS" BASIS,        *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and      *
+ *  limitations under the License.                                           *
+ *                                                                           *
+ ****************************************************************************/
+
+
+/*============================ INCLUDES ======================================*/
+
+#include "hal/vsf_hal_cfg.h"
+
+#if VSF_HAL_USE_DAC == ENABLED
+
+/*\note For IPCore drivers, define __VSF_HAL_${DAC_IP}_DAC_CLASS_IMPLEMENT before including vsf_hal.h.
+ *      For peripherial drivers, if IPCore driver is used, define __VSF_HAL_${DAC_IP}_DAC_CLASS_INHERIT__ before including vsf_hal.h
+ */
+
+// IPCore
+#define __VSF_HAL_${DAC_IP}_DAC_CLASS_IMPLEMENT
+// IPCore end
+// HW using ${DAC_IP} IPCore driver
+#define __VSF_HAL_${DAC_IP}_DAC_CLASS_INHERIT__
+// HW end
+
+#include "hal/vsf_hal.h"
+
+/*============================ MACROS ========================================*/
+
+/*\note VSF_HW_DAC_CFG_MULTI_CLASS is only for drivers for specified device(hw drivers).
+ *      For other drivers, plase define VSF_${DAC_IP}_DAC_CFG_MULTI_CLASS in header file.
+ */
+
+// HW
+#ifndef VSF_HW_DAC_CFG_MULTI_CLASS
+#   define VSF_HW_DAC_CFG_MULTI_CLASS           VSF_DAC_CFG_MULTI_CLASS
+#endif
+// HW end
+
+// HW
+#define VSF_DAC_CFG_IMP_PREFIX                  vsf_hw
+#define VSF_DAC_CFG_IMP_UPCASE_PREFIX           VSF_HW
+// HW end
+// IPCore
+#define VSF_DAC_CFG_IMP_PREFIX                  vsf_${dac_ip}
+#define VSF_DAC_CFG_IMP_UPCASE_PREFIX           VSF_${DAC_IP}
+// IPCore end
+
+/*============================ TYPES =========================================*/
+
+// HW
+typedef struct VSF_MCONNECT(VSF_DAC_CFG_IMP_PREFIX, _dac_t) {
+#if VSF_HW_DAC_CFG_MULTI_CLASS == ENABLED
+    vsf_dac_t               vsf_dac;
+#endif
+    void                    *reg;
+    vsf_dac_isr_t           isr;
+} VSF_MCONNECT(VSF_DAC_CFG_IMP_PREFIX, _dac_t);
+// HW end
+
+
+/*============================ MACROS ========================================*/
+/*============================ MACROFIED FUNCTIONS ===========================*/
+/*============================ TYPES =========================================*/
+/*============================ GLOBAL VARIABLES ==============================*/
+/*============================ LOCAL VARIABLES ===============================*/
+/*============================ PROTOTYPES ====================================*/
+
+/*============================ IMPLEMENTATION ================================*/
+
+/*\note Implementation below is for hw dac only, because there is no requirements
+ *          on the APIs of IPCore drivers.
+ *      Usage of VSF_MCONNECT is not a requirement, but for convenience only,
+ */
+
+// HW
+vsf_err_t VSF_MCONNECT(VSF_DAC_CFG_IMP_PREFIX, _dac_init)(
+    VSF_MCONNECT(VSF_DAC_CFG_IMP_PREFIX, _dac_t) *dac_ptr,
+    vsf_dac_cfg_t* cfg_ptr
+) {
+    VSF_HAL_ASSERT((NULL != dac_ptr) && (NULL != cfg_ptr));
+    // configure according to cfg_ptr
+    dac_ptr->isr = cfg_ptr->isr;
+    // configure interrupt according to cfg_ptr->isr
+    return VSF_ERR_NONE;
+}
+
+void VSF_MCONNECT(VSF_DAC_CFG_IMP_PREFIX, _dac_fini)(
+    VSF_MCONNECT(VSF_DAC_CFG_IMP_PREFIX, _dac_t) *dac_ptr
+) {
+    VSF_HAL_ASSERT(dac_ptr != NULL);
+}
+
+fsm_rt_t VSF_MCONNECT(VSF_DAC_CFG_IMP_PREFIX, _dac_enable)(
+    VSF_MCONNECT(VSF_DAC_CFG_IMP_PREFIX, _dac_t) *dac_ptr
+) {
+    VSF_HAL_ASSERT(dac_ptr != NULL);
+
+    return fsm_rt_cpl;
+}
+
+fsm_rt_t VSF_MCONNECT(VSF_DAC_CFG_IMP_PREFIX, _dac_disable)(
+    VSF_MCONNECT(VSF_DAC_CFG_IMP_PREFIX, _dac_t) *dac_ptr
+) {
+    VSF_HAL_ASSERT(dac_ptr != NULL);
+
+    return fsm_rt_cpl;
+}
+
+void VSF_MCONNECT(VSF_DAC_IRQ_CFG_IMP_PREFIX, _dac_irq_enable)(
+    VSF_MCONNECT(VSF_DAC_CFG_IMP_PREFIX, _dac_t) *dac_ptr,
+    vsf_dac_irq_mask_t irq_mask
+) {
+    VSF_HAL_ASSERT(dac_ptr != NULL);
+}
+
+void VSF_MCONNECT(VSF_DAC_IRQ_CFG_IMP_PREFIX, _dac_irq_disable)(
+    VSF_MCONNECT(VSF_DAC_CFG_IMP_PREFIX, _dac_t) *dac_ptr,
+    vsf_dac_irq_mask_t irq_mask
+) {
+    VSF_HAL_ASSERT(dac_ptr != NULL);
+}
+
+vsf_dac_status_t VSF_MCONNECT(VSF_DAC_CFG_IMP_PREFIX, _dac_status)(
+    VSF_MCONNECT(VSF_DAC_CFG_IMP_PREFIX, _dac_t) *dac_ptr
+) {
+    VSF_HAL_ASSERT(NULL != dac_ptr);
+    return (vsf_dac_status_t) {
+        .is_busy = false,
+    };
+}
+
+vsf_dac_capability_t VSF_MCONNECT(VSF_DAC_CFG_IMP_PREFIX, _dac_capability)(
+    VSF_MCONNECT(VSF_DAC_CFG_IMP_PREFIX, _dac_t) *dac_ptr
+) {
+    return (vsf_dac_capability_t) {
+        .max_resolution_bits = 8,
+        .min_resolution_bits = 4,
+        .channel_count       = 4,
+    };
+}
+
+vsf_err_t VSF_MCONNECT(VSF_DAC_CHANNEL_REQUEST_CFG_IMP_PREFIX, _dac_channel_request_once)(
+    VSF_MCONNECT(VSF_DAC_CFG_IMP_PREFIX, _dac_t) *dac_ptr,
+    vsf_dac_channel_cfg_t *channel_cfg,
+    uint_fast16_t value
+) {
+    VSF_HAL_ASSERT(dac_ptr != NULL);
+    VSF_HAL_ASSERT(channel_cfg != NULL);
+
+    return VSF_ERR_NONE;
+}
+
+vsf_err_t VSF_MCONNECT(VSF_DAC_CHANNEL_CFG_IMP_PREFIX, _dac_channel_config)(
+    VSF_MCONNECT(VSF_DAC_CFG_IMP_PREFIX, _dac_t) *dac_ptr,
+    vsf_dac_channel_cfg_t *channel_cfgs_ptr,
+    uint_fast8_t channel_cfgs_cnt
+) {
+    VSF_HAL_ASSERT(dac_ptr != NULL);
+    VSF_HAL_ASSERT(channel_cfgs_ptr != NULL);
+    VSF_HAL_ASSERT(channel_cfgs_cnt != 0);
+
+    return VSF_ERR_NONE;
+}
+
+vsf_err_t VSF_MCONNECT(VSF_DAC_CHANNEL_CFG_IMP_PREFIX, _dac_channel_request)(
+    VSF_MCONNECT(VSF_DAC_CFG_IMP_PREFIX, _dac_t) *dac_ptr,
+    void *buffer_ptr,
+    uint_fast32_t count
+) {
+    VSF_HAL_ASSERT(dac_ptr != NULL);
+    VSF_HAL_ASSERT(buffer_ptr != NULL);
+    VSF_HAL_ASSERT(count != 0);
+
+    return VSF_ERR_NONE;
+}
+
+static void VSF_MCONNECT(__, VSF_DAC_CFG_IMP_PREFIX, _dac_irqhandler)(
+    VSF_MCONNECT(VSF_DAC_CFG_IMP_PREFIX, _dac_t) *dac_ptr
+) {
+    VSF_HAL_ASSERT(NULL != dac_ptr);
+
+    vsf_dac_irq_mask_t irq_mask = GET_IRQ_MASK(dac_ptr);
+    vsf_dac_isr_t *isr_ptr = &dac_ptr->isr;
+    if ((irq_mask != 0) && (isr_ptr->handler_fn != NULL)) {
+        isr_ptr->handler_fn(isr_ptr->target_ptr, ac_ptr, irq_mask);
+    }
+}
+// HW end
+
+/*============================ MACROFIED FUNCTIONS ===========================*/
+
+/*\note Instantiation below is for hw dac only.
+ *      Usage of VSF_MCONNECT is not a requirement, but for convenience only,
+ */
+
+// HW
+#define VSF_DAC_CFG_IMP_LV0(__IDX, __HAL_OP)                                    \
+    VSF_MCONNECT(VSF_DAC_CFG_IMP_PREFIX, _dac_t)                                \
+        VSF_MCONNECT(VSF_DAC_CFG_IMP_PREFIX, _dac, __IDX) = {                   \
+        .reg                = VSF_MCONNECT(VSF_DAC_CFG_IMP_UPCASE_PREFIX, _DAC, __IDX,_REG_),\
+        __HAL_OP                                                                \
+    };                                                                          \
+    VSF_CAL_ROOT void VSF_MCONNECT(VSF_DAC_CFG_IMP_UPCASE_PREFIX, _DAC, __IDX, _IRQHandler)(void)\
+    {                                                                           \
+        uintptr_t ctx = vsf_hal_irq_enter();                                    \
+        VSF_MCONNECT(__, VSF_DAC_CFG_IMP_PREFIX, _dac_irqhandler)(              \
+            &VSF_MCONNECT(VSF_DAC_CFG_IMP_PREFIX, _dac, __IDX)                  \
+        );                                                                      \
+        vsf_hal_irq_leave(ctx);                                                 \
+    }
+#include "hal/driver/common/dac/dac_template.inc"
+// HW end
+
+#endif /* VSF_HAL_USE_DAC */
---
+++ source/hal/driver/template/__series_name_a__/common/dac/dac.h
@@ -0,0 +1,107 @@
+/*****************************************************************************
+ *   Copyright(C)2009-2022 by VSF Team                                       *
+ *                                                                           *
+ *  Licensed under the Apache License, Version 2.0 (the "License");          *
+ *  you may not use this file except in compliance with the License.         *
+ *  You may obtain a copy of the License at                                  *
+ *                                                                           *
+ *     http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                           *
+ *  Unless required by applicable law or agreed to in writing, software      *
+ *  distributed under the License is distributed on an "AS IS" BASIS,        *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and      *
+ *  limitations under the License.                                           *
+ *                                                                           *
+ ****************************************************************************/
+
+#ifndef __HAL_DRIVER_${SERIES/DAC_IP}_DAC_H__
+#define __HAL_DRIVER_${SERIES/DAC_IP}_DAC_H__
+
+/*============================ INCLUDES ======================================*/
+
+#include "hal/vsf_hal_cfg.h"
+
+#if VSF_HAL_USE_DAC == ENABLED
+
+#include "../../__device.h"
+
+/*\note Refer to template/README.md for usage cases.
+ *      For peripherial drivers, blackbox mode is recommended but not required, reimplementation part MUST be open.
+ *      For IPCore drivers, class structure, MULTI_CLASS configuration, reimplementation and class APIs should be open to user.
+ *      For emulated drivers, **** No reimplementation ****.
+ */
+
+/*\note Incudes CAN ONLY be put here. */
+/*\note If current header is for a peripherial driver(hw driver), and inherit from an IPCore driver, include IPCore header here. */
+
+// IPCore
+#if     defined(__VSF_HAL_${DAC_IP}_DAC_CLASS_IMPLEMENT)
+#   define __VSF_CLASS_IMPLEMENT__
+#elif   defined(__VSF_HAL_${DAC_IP}_DAC_CLASS_INHERIT__)
+#   define __VSF_CLASS_INHERIT__
+#endif
+
+#include "utilities/ooc_class.h"
+// IPCore end
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*============================ MACROS ========================================*/
+
+/*\note VSF_${DAC_IP}_DAC_CFG_MULTI_CLASS should be implemented for IP drives and open to user,
+ *          while VSF_HW_DAC_CFG_MULTI_CLASS should be in dac.c.
+ */
+
+// IPCore
+#ifndef VSF_${DAC_IP}_DAC_CFG_MULTI_CLASS
+#   define VSF_${DAC_IP}_DAC_CFG_MULTI_CLASS    VSF_DAC_CFG_MULTI_CLASS
+#endif
+// IPCore end
+
+/*============================ MACROFIED FUNCTIONS ===========================*/
+/*============================ TYPES =========================================*/
+
+// IPCore
+vsf_class(vsf_${dac_ip}_dac_t) {
+#if VSF_${DAC_IP}_CFG_MULTI_CLASS == ENABLED
+    public_member(
+        vsf_dac_t               vsf_dac;
+    )
+#endif
+
+/*\note You can add more memmber in vsf_${dac_ip}_dac_t instance.
+ *      For members accessable from child, put in protected_member.
+ *      Else, put in private_member.
+ */
+
+    protected_member(
+        vsf_${dac_ip}_dac_reg_t *reg;
+        vsf_dac_isr_t           isr;
+    )
+};
+// IPCore end
+
+/*============================ INCLUDES ======================================*/
+
+// IPCore
+/*\note Extern APIs for ip core diriver.
+ *      There is no requirement about how APIs of IPCore drivers should be implemented.
+ *      Just consider the simplicity for actual peripherial drivers.
+ */
+// IPCore end
+
+#ifdef __cplusplus
+}
+#endif
+
+// IPCore
+#undef __VSF_HAL_${DAC_IP}_DAC_CLASS_IMPLEMENT
+#undef __VSF_HAL_${DAC_IP}_DAC_CLASS_INHERIT__
+// IPCore end
+
+#endif      // VSF_HAL_USE_DAC
+#endif      // __HAL_DRIVER_${SERIES/DAC_IP}_DAC_H__
+/* EOF */
