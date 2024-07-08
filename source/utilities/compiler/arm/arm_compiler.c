349919339ca401f984fd5fb74c0c48f4e7e562e0
vsfos <vsfos@foxmail.com>
2024-07-08 15:44:14 +0800

    [utilities/compiler/arm] implement itoa weak version in compiler layer

--- source/utilities/compiler/arm/arm_compiler.c
+++ source/utilities/compiler/arm/arm_compiler.c
@@ -58,6 +58,41 @@ size_t strnlen(const char *str, size_t maxlen)
 	for (sc = str; maxlen-- && *sc != '\0'; ++sc);
 	return sc - str;
 }
+
+VSF_CAL_WEAK(itoa)
+char * itoa(int num, char *str, int radix)
+{
+    char index[] = "0123456789ABCDEF";
+    unsigned unum;
+    int i = 0, j, k;
+
+    if (radix == 10 && num < 0) {
+        unum = (unsigned)-num;
+        str[i++] = '-';
+    } else {
+        unum = (unsigned)num;
+    }
+
+    do{
+        str[i++] = index[unum % (unsigned)radix];
+        unum /= radix;
+   } while (unum);
+
+    str[i] = '\0';
+    if (str[0] == '-') {
+        k = 1;
+    } else {
+        k = 0;
+    }
+
+    for (j = k; j <= (i - 1) / 2; j++) {
+        char temp;
+        temp = str[j];
+        str[j] = str[i - 1 + k - j];
+        str[i - 1 + k - j] = temp;
+    }
+    return str;
+}
 #endif
 
 #if __IS_COMPILER_IAR__
