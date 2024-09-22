ee1f2b8258f3fa8b341d913fb0968ffd56d90149
vsfos <vsfos@foxmail.com>
2024-09-22 20:24:28 +0800

    [shell/linux] fix typo in previous commit

--- source/shell/sys/linux/vsf_linux.c
+++ source/shell/sys/linux/vsf_linux.c
@@ -4709,9 +4709,9 @@ int sysinfo(struct sysinfo *info)
     vsf_heap_statistics_t heap_statistics;
 #   if VSF_LINUX_CFG_HEAP_SIZE > 0
     __vsf_heap_statistics(&__vsf_linux_heap.use_as__vsf_heap_t, &heap_statistics);
-#   elif VSF_ARCH_PROVIDE_HEAP != ENABLED || VSF_ARCH_HEAP_HAS_STATISTICS == ENABLED)
+#   elif VSF_ARCH_PROVIDE_HEAP != ENABLED || VSF_ARCH_HEAP_HAS_STATISTICS == ENABLED
     vsf_heap_statistics(&heap_statistics);
-#endif
+#   endif
     info->totalram = heap_statistics.all_size;
     info->freeram = heap_statistics.all_size - heap_statistics.used_size;
 #endif
