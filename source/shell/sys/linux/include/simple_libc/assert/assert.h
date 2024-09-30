ff4a015b23ad4cab114717ede4f39007d1576754
vsfos <vsfos@foxmail.com>
2024-10-01 02:20:14 +0800

    [shell/linux] add _Static_assert and static_assert in assert.h

--- source/shell/sys/linux/include/simple_libc/assert/assert.h
+++ source/shell/sys/linux/include/simple_libc/assert/assert.h
@@ -8,10 +8,13 @@ extern "C" {
 #undef assert
 
 #ifdef NDEBUG
-#   define assert(...)          ((void)0)
+#   define assert(...)                  ((void)0)
 #else
-#   define assert               VSF_ASSERT
+#   define assert                       VSF_ASSERT
 #endif
+
+#define _Static_assert(__expr, ...)     VSF_STATIC_ASSERT(__expr)
+#define static_assert                   _Static_assert
 
 #ifdef __cplusplus
 }
