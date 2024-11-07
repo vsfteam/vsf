b783af9842994ffd20d0b0a3826b7040962a79c0
Yongxiang Liang <tanekliang@gmail.com>
2024-11-07 23:04:41 +0800

    [component/input/cst8] update because of vsf_gpio_templte

--- source/component/input/driver/touch_screen/hynitron/cst8/vsf_input_cst8.c
+++ source/component/input/driver/touch_screen/hynitron/cst8/vsf_input_cst8.c
@@ -96,7 +96,7 @@ static void __vk_input_cst8_i2c_isrhandler(void *target_ptr, vsf_i2c_t *i2c_ptr,
                 vsf_input_on_touchscreen(&ts_evt);
             }
         enable_exti:
-            vsf_gpio_exti_irq_enable(cst8->gpio_int_port, 1 << cst8->gpio_int_pin_idx, cst8->prio);
+            vsf_gpio_exti_irq_enable(cst8->gpio_int_port, 1 << cst8->gpio_int_pin_idx);
             break;
         }
     }
@@ -125,7 +125,7 @@ vsf_err_t vk_input_cst8_init(vk_input_cst8_t *cst8, vsf_arch_prio_t prio)
 
     };
     vsf_gpio_port_config_pins(cst8->gpio_int_port, 1 << cst8->gpio_int_pin_idx, &cfg);
-    vsf_gpio_exti_irq_pin_config(cst8->gpio_int_port, 1 << cst8->gpio_int_pin_idx, &(vsf_gpio_pin_irq_cfg_t){
+    vsf_gpio_exti_irq_pin_config(cst8->gpio_int_port, 1 << cst8->gpio_int_pin_idx, &(vsf_gpio_exti_irq_cfg_t){
         .handler_fn = __vk_input_cst8_eint_isrhandler,
         .target_ptr = cst8,
         .prio = cst8->prio,
