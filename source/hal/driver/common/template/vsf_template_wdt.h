098fde9158845c8f5e27d4a22844d20546315f13
Yongxiang Liang <tanekliang@gmail.com>
2025-02-12 12:37:41 +0800

    [hal/driver/vsf_template_wdt] fix typo and add more comment

--- source/hal/driver/common/template/vsf_template_wdt.h
+++ source/hal/driver/common/template/vsf_template_wdt.h
@@ -79,8 +79,8 @@ extern "C" {
 #    define VSF_WDT_CFG_REIMPLEMENT_TYPE_CAPABILITY DISABLED
 #endif
 
-#ifndef VSF_WDT_CFG_INHERT_HAL_CAPABILITY
-#   define VSF_WDT_CFG_INHERT_HAL_CAPABILITY            ENABLED
+#ifndef VSF_WDT_CFG_INHERIT_HAL_CAPABILITY
+#   define VSF_WDT_CFG_INHERIT_HAL_CAPABILITY            ENABLED
 #endif
 
 /*============================ MACROFIED FUNCTIONS ===========================*/
@@ -103,22 +103,25 @@ typedef enum vsf_wdt_mode_t {
     VSF_WDT_MODE_RESET_NONE      = (0 << 1),
     VSF_WDT_MODE_RESET_CPU       = (1 << 1),
     VSF_WDT_MODE_RESET_SOC       = (2 << 1),
+
+    /*
+    // optional: prescaler mode
+    VSF_WDT_MODE_PRESCALER_1        = (0 << 2),
+    VSF_WDT_MODE_PRESCALER_2        = (1 << 2),
+    VSF_WDT_MODE_PRESCALER_4        = (2 << 2),
+    VSF_WDT_MODE_PRESCALER_8        = (3 << 2),
+    #define VSF_WDT_MODE_PRESCALER_MASK  (0x03 << 2)
+    */
 } vsf_wdt_mode_t;
 #endif
 
 enum {
-    VSF_WDT_MODE_EARLY_WAKEUP_COUNT = 2,
     VSF_WDT_MODE_EARLY_WAKEUP_MASK  = VSF_WDT_MODE_NO_EARLY_WAKEUP |
                                       VSF_WDT_MODE_EARLY_WAKEUP,
 
-    VSF_WDT_MODE_RESET_COUNT        = 3,
     VSF_WDT_MODE_RESET_MASK         = VSF_WDT_MODE_RESET_NONE |
                                       VSF_WDT_MODE_RESET_CPU |
                                       VSF_WDT_MODE_RESET_CPU,
-
-    VSF_WDT_MODE_MASK_COUNT         = 2,
-    VSF_WDT_MODE_ALL_BITS_MASK      = VSF_WDT_MODE_EARLY_WAKEUP_MASK |
-                                      VSF_WDT_MODE_RESET_MASK,
 };
 
 #if VSF_WDT_CFG_REIMPLEMENT_TYPE_CFG == DISABLED
@@ -141,7 +144,7 @@ typedef struct vsf_wdt_cfg_t {
 
 #if VSF_WDT_CFG_REIMPLEMENT_TYPE_CAPABILITY == DISABLED
 typedef struct vsf_wdt_capability_t {
-#if VSF_WDT_CFG_INHERT_HAL_CAPABILITY == ENABLED
+#if VSF_WDT_CFG_INHERIT_HAL_CAPABILITY == ENABLED
     inherit(vsf_peripheral_capability_t)
 #endif
 
@@ -199,38 +202,33 @@ extern vsf_err_t vsf_wdt_init(vsf_wdt_t *wdt_ptr, vsf_wdt_cfg_t *cfg_ptr);
  \~chinese
  @brief 终止一个 wdt 实例
  @param[in] wdt_ptr: 结构体 vsf_wdt_t 的指针，参考 @ref vsf_wdt_t
- @param[in] cfg_ptr: 结构体 vsf_wdt_cfg_t 的指针，参考 @ref vsf_wdt_cfg_t
  @return 无。
  */
 extern void vsf_wdt_fini(vsf_wdt_t *wdt_ptr);
 
 /**
  \~english
- @brief enable interrupt masks of wdt instance.
+ @brief enable a wdt instance.
  @param[in] wdt_ptr: a pointer to structure @ref vsf_wdt_t
- @param[in] irq_mask: one or more value of enum @ref vsf_wdt_irq_mask_t
- @return none.
+ @return fsm_rt_t: return FSM_RT_CPL if wdt was enabled, return FSM_RT_ONGOING if wdt is enabling
 
  \~chinese
- @brief 使能 wdt 实例的中断
+ @brief 使能一个 wdt 实例
  @param[in] wdt_ptr: 结构体 vsf_wdt_t 的指针，参考 @ref vsf_wdt_t
- @param[in] irq_mask: 一个或者多个枚举 vsf_wdt_irq_mask_t 的值的按位或，@ref vsf_wdt_irq_mask_t
- @return 无。
+ @return fsm_rt_t: 返回 FSM_RT_CPL 如果 wdt 实例被使能，返回 FSM_RT_ONGOING 如果 wdt 实例正在使能
  */
 extern fsm_rt_t vsf_wdt_enable(vsf_wdt_t *wdt_ptr);
 
 /**
  \~english
- @brief disable interrupt masks of wdt instance.
+ @brief disable a wdt instance.
  @param[in] wdt_ptr: a pointer to structure @ref vsf_wdt_t
- @param[in] irq_mask: one or more value of enum vsf_wdt_irq_mask_t, @ref vsf_wdt_irq_mask_t
- @return none.
+ @return fsm_rt_t: return FSM_RT_CPL if wdt was disabled, return FSM_RT_ONGOING if wdt is disabling
 
  \~chinese
- @brief 禁能 wdt 实例的中断
+ @brief 禁能一个 wdt 实例
  @param[in] wdt_ptr: 结构体 vsf_wdt_t 的指针，参考 @ref vsf_wdt_t
- @param[in] irq_mask: 一个或者多个枚举 vsf_wdt_irq_mask_t 的值的按位或，@ref vsf_wdt_irq_mask_t
- @return 无。
+ @return fsm_rt_t: 返回 FSM_RT_CPL 如果 wdt 实例被禁能，返回 FSM_RT_ONGOING 如果 wdt 实例正在禁能
  */
 extern fsm_rt_t vsf_wdt_disable(vsf_wdt_t *wdt_ptr);
 
