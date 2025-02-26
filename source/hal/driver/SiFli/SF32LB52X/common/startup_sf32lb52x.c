5aadbe47a6a6fc9524a8885f295045ebef7a06f1
vsfos <vsfos@foxmail.com>
2025-02-26 13:56:01 +0800

    [hal/sf32lb5xx] add VSF_HW_INTERRUPTS

--- source/hal/driver/SiFli/SF32LB52X/common/startup_sf32lb52x.c
+++ source/hal/driver/SiFli/SF32LB52X/common/startup_sf32lb52x.c
@@ -59,107 +59,6 @@ __NO_RETURN Reset_Handler(void);
   Exception / Interrupt Handler
  *----------------------------------------------------------------------------*/
 
-#define __INTERRUPTS                                                            \
-    AON_IRQHandler,                                                             \
-    ble_isr,                                                                    \
-    DMAC2_CH1_IRQHandler,                                                       \
-    DMAC2_CH2_IRQHandler,                                                       \
-    DMAC2_CH3_IRQHandler,                                                       \
-    DMAC2_CH4_IRQHandler,                                                       \
-    DMAC2_CH5_IRQHandler,                                                       \
-    DMAC2_CH6_IRQHandler,                                                       \
-    DMAC2_CH7_IRQHandler,                                                       \
-    DMAC2_CH8_IRQHandler,                                                       \
-    PATCH_IRQHandler,                                                           \
-    dm_isr,                                                                     \
-    USART4_IRQHandler,                                                          \
-    USART5_IRQHandler,                                                          \
-    SECU2_IRQHandler,                                                           \
-    bt_isr,                                                                     \
-    BTIM3_IRQHandler,                                                           \
-    BTIM4_IRQHandler,                                                           \
-    PTC2_IRQHandler,                                                            \
-    LPTIM3_IRQHandler,                                                          \
-    GPIO2_IRQHandler,                                                           \
-    HPSYS0_IRQHandler,                                                          \
-    HPSYS1_IRQHandler,                                                          \
-    SWI0_IRQHandler,                                                            \
-    SWI1_IRQHandler,                                                            \
-    SWI2_IRQHandler,                                                            \
-    SWI3_IRQHandler,                                                            \
-    SWI4_IRQHandler,                                                            \
-    SWI5_IRQHandler,                                                            \
-    SWI6_IRQHandler,                                                            \
-    SWI7_IRQHandler,                                                            \
-    SWI8_IRQHandler,                                                            \
-    SWI9_IRQHandler,                                                            \
-    SWI10_IRQHandler,                                                           \
-    SWI11_IRQHandler,                                                           \
-    SWI12_IRQHandler,                                                           \
-    SWI13_IRQHandler,                                                           \
-    SWI14_IRQHandler,                                                           \
-    SWI15_IRQHandler,                                                           \
-    SWI16_IRQHandler,                                                           \
-    SWI17_IRQHandler,                                                           \
-    SWI18_IRQHandler,                                                           \
-    SWI19_IRQHandler,                                                           \
-    SWI20_IRQHandler,                                                           \
-    SWI21_IRQHandler,                                                           \
-    SWI22_IRQHandler,                                                           \
-    LPTIM1_IRQHandler,                                                          \
-    LPTIM2_IRQHandler,                                                          \
-    PMUC_IRQHandler,                                                            \
-    RTC_IRQHandler,                                                             \
-    DMAC1_CH1_IRQHandler,                                                       \
-    DMAC1_CH2_IRQHandler,                                                       \
-    DMAC1_CH3_IRQHandler,                                                       \
-    DMAC1_CH4_IRQHandler,                                                       \
-    DMAC1_CH5_IRQHandler,                                                       \
-    DMAC1_CH6_IRQHandler,                                                       \
-    DMAC1_CH7_IRQHandler,                                                       \
-    DMAC1_CH8_IRQHandler,                                                       \
-    LCPU2HCPU_IRQHandler,                                                       \
-    USART1_IRQHandler,                                                          \
-    SPI1_IRQHandler,                                                            \
-    I2C1_IRQHandler,                                                            \
-    EPIC_IRQHandler,                                                            \
-    LCDC1_IRQHandler,                                                           \
-    I2S1_IRQHandler,                                                            \
-    GPADC_IRQHandler,                                                           \
-    EFUSEC_IRQHandler,                                                          \
-    AES_IRQHandler,                                                             \
-    PTC1_IRQHandler,                                                            \
-    TRNG_IRQHandler,                                                            \
-    GPTIM1_IRQHandler,                                                          \
-    GPTIM2_IRQHandler,                                                          \
-    BTIM1_IRQHandler,                                                           \
-    BTIM2_IRQHandler,                                                           \
-    USART2_IRQHandler,                                                          \
-    SPI2_IRQHandler,                                                            \
-    I2C2_IRQHandler,                                                            \
-    EXTDMA_IRQHandler,                                                          \
-    I2C4_IRQHandler,                                                            \
-    SDMMC1_IRQHandler,                                                          \
-    SWI23_IRQHandler,                                                           \
-    SWI24_IRQHandler,                                                           \
-    PDM1_IRQHandler,                                                            \
-    SWI25_IRQHandler,                                                           \
-    GPIO1_IRQHandler,                                                           \
-    QSPI1_IRQHandler,                                                           \
-    QSPI2_IRQHandler,                                                           \
-    SWI26_IRQHandler,                                                           \
-    SWI27_IRQHandler,                                                           \
-    EZIP_IRQHandler,                                                            \
-    AUDPRC_IRQHandler,                                                          \
-    TSEN_IRQHandler,                                                            \
-    USBC_IRQHandler,                                                            \
-    I2C3_IRQHandler,                                                            \
-    ATIM1_IRQHandler,                                                           \
-    USART3_IRQHandler,                                                          \
-    AUD_HP_IRQHandler,                                                          \
-    SWI28_IRQHandler,                                                           \
-    SECU1_IRQHandler
-
 #define __imp_blocked_weak_handler(__name)                                      \
             __WEAK void __name(void){while(1);}
 
@@ -175,7 +74,7 @@ VSF_MFOREACH(__imp_blocked_weak_handler,
     PendSV_Handler,
     SysTick_Handler,
 
-    __INTERRUPTS
+    VSF_HW_INTERRUPTS
 )
 
 /*----------------------------------------------------------------------------
@@ -206,7 +105,7 @@ __USED const pFunc __VECTOR_TABLE[] __VECTOR_TABLE_ATTRIBUTE = {
     SysTick_Handler,                          /*  -1 SysTick Handler */
 
     /* Interrupts */
-    __INTERRUPTS
+    VSF_HW_INTERRUPTS
 };
 
 #if defined ( __GNUC__ )
