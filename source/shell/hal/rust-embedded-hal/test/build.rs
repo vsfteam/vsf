98c53e7165a420b6f50f956a943acd659a33fa56
SimonQian <vsfos@qq.com>
2025-10-05 14:06:06 +0800

    [shell/rust-embedded-hal] avoid to copy memory.x in build.rs of test

--- source/shell/hal/rust-embedded-hal/test/build.rs
+++ source/shell/hal/rust-embedded-hal/test/build.rs
@@ -1,13 +1,12 @@
 use std::env;
 use std::path::Path;
-use std::fs;
 
 fn main() {
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let model = env::var("VSF_MODEL").expect("please define VSF_MODEL in config.toml [env] section");
-    let model_memory_path = Path::new(&manifest_dir).join(format!("memory/{model}/memory.x"));
-    fs::copy(model_memory_path, "./memory.x").expect(&format!("fail to find memory.x for {model}"));
+    let model_memory_path = Path::new(&manifest_dir).join(format!("memory/{model}"));
 
+    println!("cargo:rustc-link-search={}", model_memory_path.display());
     println!("cargo:rustc-link-arg-bins=--nmagic");
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
     println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
