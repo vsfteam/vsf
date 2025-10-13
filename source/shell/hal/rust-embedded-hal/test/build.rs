0ccb96c3355befc25a0491d6cf1a2de270222d9c
SimonQian <vsfos@qq.com>
2025-10-13 15:06:17 +0800

    [shell/rust-embedded-hal] bug fix in build.rs of test while detect hostos

--- source/shell/hal/rust-embedded-hal/test/build.rs
+++ source/shell/hal/rust-embedded-hal/test/build.rs
@@ -2,16 +2,13 @@ use std::env;
 
 fn main() {
     let arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
-    let mut hostos = false;
+    let os = env::var("CARGO_CFG_TARGET_OS").unwrap();
     println!("cargo:warning=target_arch: {arch}");
-    if let Ok(os) = env::var("CARGO_CFG_TARGET_OS") {
-        println!("cargo:warning=target_os: {os}");
-        hostos = true;
-    }
+    println!("cargo:warning=target_os: {os}");
 
     println!("cargo::rustc-check-cfg=cfg(use_defmt_rtt)");
 
-    if !hostos {
+    if os == "" || os == "none" {
         println!("cargo:rustc-link-arg-bins=--nmagic");
         println!("cargo:rustc-link-arg-bins=-Tlink.x");
         println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
