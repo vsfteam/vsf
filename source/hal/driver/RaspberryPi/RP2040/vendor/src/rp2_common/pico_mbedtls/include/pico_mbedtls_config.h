068b8cf8ef1928a60267485806d3adea31b742ca
Yongxiang Liang <tanekliang@gmail.com>
2026-06-03 11:44:44 +0800

    [hal/driver/RP2040] fix vendor: convert copied files to proper git submodule

--- source/hal/driver/RaspberryPi/RP2040/vendor/src/rp2_common/pico_mbedtls/include/pico_mbedtls_config.h
+++
@@ -1,3 +0,0 @@
-// Latest versions of mbedtls include mbedtls/include/mbedtls_config.h and we used to set MBEDTLS_CONFIG_FILE=mbedtls_config.h
-// To maintain compatibility with this and avoid including the mbedtls version of mbedtls_config.h we include pico_mbedtls_config.h first
-#include "mbedtls_config.h"
